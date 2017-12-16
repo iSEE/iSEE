11f6fedf4f72567e2385bd99a49d32df630ec98b
Aaron Lun <alun@wehi.edu.au>
2017-12-16 16:42:30 +0000

    Fixes to Imports to pass CHECK.

--- R/iSEE-pkg.R
+++ R/iSEE-pkg.R
@@ -9,12 +9,12 @@
 #' @importFrom rintrojs introjsUI
 #' @importFrom shiny actionButton fluidRow icon reactiveValues renderUI
 #' shinyApp tabPanel HTML br h1 h2 selectInput renderPlot plotOutput 
-#' uiOutput sliderInput
+#' uiOutput sliderInput tagList
 #' @importFrom shinydashboard dashboardBody dashboardHeader dashboardPage
 #' dashboardSidebar menuItem tabBox valueBox valueBoxOutput
 #' @importFrom utils packageVersion
 #' @importFrom shiny tags
-#' @importFrom ggplot2 ggplot geom_point labs theme_void
+#' @importFrom ggplot2 ggplot geom_point labs theme_void aes_string
 #'
 #' @author Aaron Lun \email{alun@@wehi.edu.au}
 #' @author Charlotte Soneson \email{charlotte.soneson@@uzh.ch}

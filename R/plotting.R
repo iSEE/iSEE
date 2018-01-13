fa1fb61f52dbf955257688ce7e03cebda16ef000
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-01-13 16:55:56 +0000

    colDataPlot independent of scater

--- R/plotting.R
+++ R/plotting.R
@@ -1,6 +1,6 @@
-.make_redDimPlot <- function(se, param_choices, input, all.coordinates) 
+.make_redDimPlot <- function(se, param_choices, input, all.coordinates)
 # Makes the dimension reduction plot.
-{ 
+{
     red.dim <- reducedDim(se, param_choices[[.redDimType]])
 
     color_choice <- param_choices[[.colorByField]]
@@ -16,25 +16,25 @@
         assay.choice <- param_choices[[.colorByGeneTableAssay]]
       } else {
         covariate.name <- param_choices[[.colorByGeneText]]
-        if (!covariate.name %in% rownames(se)) { 
+        if (!covariate.name %in% rownames(se)) {
           covariate.name <- NULL
         }
         assay.choice <- param_choices[[.colorByGeneTextAssay]]
       }
-      if (!is.null(covariate.name)) { 
+      if (!is.null(covariate.name)) {
         covariate <- assay(se, assay.choice)[covariate.name,]
         astr <- "aes(x=Dim1, y=Dim2, color=Covariate)"
-        cov.str <- sprintf(";\nplot.data$Covariate <- assay(se, '%s')['%s',]", 
+        cov.str <- sprintf(";\nplot.data$Covariate <- assay(se, '%s')['%s',]",
                            assay.choice, covariate.name)
       } else {
         covariate.name <- ""
-        covariate <- NULL              
+        covariate <- NULL
         astr <- "aes(x=Dim1, y=Dim2)"
         cov.str <- ""
         }
     } else {
       covariate.name <- ""
-      covariate <- NULL              
+      covariate <- NULL
       astr <- "aes(x=Dim1, y=Dim2)"
       cov.str <- ""
     }
@@ -45,26 +45,26 @@
         brush.by <- .encode_panel_name(brush.in)
         brush.id <- input[[paste0(brush.by$Type, .brushField, brush.by$ID)]]
         brushed.pts <- brushedPoints(all.coordinates[[paste0(brush.by$Type, "Plot", brush.by$ID)]], brush.id)
-        if (!is.null(brushed.pts) && nrow(brushed.pts)) { 
+        if (!is.null(brushed.pts) && nrow(brushed.pts)) {
             print("YAY, brushing!")
             print(brushed.pts)
-        } 
+        }
     }
 
     plot.data <- data.frame(Dim1=red.dim[,param_choices[[.redDimXAxis]]],
                             Dim2=red.dim[,param_choices[[.redDimYAxis]]])
     plot.data$Covariate <- covariate
-              
+
     # out.plot <- ggplot(plot.data, astr) +
     #     geom_point(size=1.5) +
     #     labs(color=covariate.name) +
     #     theme_void()
 
     #list(xy=plot.data, plot=out.plot)
-    
+
     cmd <- sprintf(
-      "red.dim <- reducedDim(se, '%s'); 
-plot.data <- data.frame(Dim1 = red.dim[, %s], 
+      "red.dim <- reducedDim(se, '%s');
+plot.data <- data.frame(Dim1 = red.dim[, %s],
 Dim2 = red.dim[, %s])%s;
 ggplot(data = plot.data, %s) + geom_point(size=1.5) + labs(color='%s') + theme_void()",
       param_choices[[.redDimType]],
@@ -77,29 +77,70 @@ ggplot(data = plot.data, %s) + geom_point(size=1.5) + labs(color='%s') + theme_v
     list(xy = plot.data, cmd = cmd, plot = eval(parse(text = cmd)))
 }
 
-.make_colDataPlot <- function(se, param_choices, input) 
+.make_colDataPlot <- function(se, param_choices, input)
 # Makes a plot of column data variables.
 {
-    aes_args <- list(y=param_choices[[.colDataYAxis]])
-#     if (param_choices[[.colDataXAxis]]!=.colDataXAxisNothingTitle) { # Currently not-quite-working as plotPhenoData needs 'x'.
-        aes_args$x <- param_choices[[.colDataXAxisColData]]
-#     }
 
-    color_choice <- param_choices[[.colorByField]]
-    if (color_choice==.colorByColDataTitle) {
-      aes_args$color <- param_choices[[.colorByColData]]
-    } else if (color_choice==.colorByGeneTableTitle) {
-      aes_args$color <- param_choices[[.colorByGeneTable]]
-    } else if (color_choice==.colorByGeneTextTitle) {
-      aes_args$color <- param_choices[[.colorByGeneText]]
+    # Process Y-axis
+    plot.data <- data.frame(
+        Y = colData(se)[,param_choices[[.colDataYAxis]]]
+    )
+
+    # Process X-axis
+    if (param_choices[[.colDataXAxis]] == .colDataXAxisNothingTitle) {
+        # TODO: allow toggling rank decreasing/increasing, note that factors cannot be negated
+        plot.data$X <- rank(plot.data$Y, ties.method = "first")
+        x_lab <- NULL
+    } else {
+        plot.data$X <- colData(se)[,param_choices[[.colDataXAxisColData]]]
+        x_lab <- param_choices[[.colDataXAxisColData]]
     }
-    aes_final <- do.call(aes_string, aes_args)
-  
-    # Creating the plot. 
-    plotPhenoData(se, aes_final)
+
+    aes_specs <- aes(X, Y, color = Covariate) # general case; color aes dropped later if applicable
+    color_choice <- param_choices[[.colorByField]]
+    if (color_choice == .colorByColDataTitle) {
+        covariate.name <- param_choices[[.colorByColData]]
+        plot.data$Covariate <- colData(se)[,covariate.name]
+    } else if (color_choice == .colorByGeneTableTitle || color_choice == .colorByGeneTextTitle){
+        if (color_choice == .colorByGeneTableTitle) {
+            covariate.name <- .find_linked_gene(se, param_choices[[.colorByGeneTable]], input)
+            assay.choice <- param_choices[[.colorByGeneTableAssay]]
+        } else if (color_choice == .colorByGeneTextTitle) {
+            covariate.name <- param_choices[[.colorByGeneText]]
+            assay.choice <- param_choices[[.colorByGeneTextAssay]]
+        } else {
+            stop("Impossible color choice!")
+        }
+        # Set the color to the selected gene
+        if (!is.null(covariate.name)){
+            plot.data$Covariate <- assay(se, assay.choice)[covariate.name,]
+        } else {
+            message("Color mode is gene expression, but none selected.")
+            aes_specs <- aes(X, Y)
+        }
+    } else {
+        covariate.name <- NULL
+        aes_specs <- aes(X, Y)
+    }
+
+    # Creating the plot.
+    gg <- ggplot(plot.data, aes_specs) +
+        geom_point() +
+        labs(
+            x = x_lab,
+            y = param_choices[[.colDataYAxis]],
+            color = covariate.name
+        ) +
+        theme_bw() +
+        theme(
+            legend.position = "bottom"
+        )
+
+    return(gg)
+    # list(xy = plot.data, cmd = cmd, plot = eval(parse(text = cmd)))
 }
 
-.make_geneExprPlot <- function(se, param_choices, input) 
+.make_geneExprPlot <- function(se, param_choices, input)
 # Makes a gene expression plot.
 {
     xchoice <- param_choices[[.geneExprXAxis]]
@@ -124,7 +165,7 @@ ggplot(data = plot.data, %s) + geom_point(size=1.5) + labs(color='%s') + theme_v
 
     # Getting the gene choice.
     cur.gene <- .find_linked_gene(se, param_choices[[.geneExprID]], input)
-    if (!is.null(cur.gene)) { 
+    if (!is.null(cur.gene)) {
         plotExpression(se, exprs_values=param_choices[[.geneExprAssay]],
                        x=byx,
                        features=cur.gene,
@@ -132,10 +173,10 @@ ggplot(data = plot.data, %s) + geom_point(size=1.5) + labs(color='%s') + theme_v
     }
 }
 
-.find_linked_gene <- function(se, link, input) 
+.find_linked_gene <- function(se, link, input)
 # Convenience function to identify the selected gene from the linked table.
-{ 
-    if (link=="") { 
+{
+    if (link=="") {
         return(NULL)
     }
     tab.id <- .encode_panel_name(link)$ID

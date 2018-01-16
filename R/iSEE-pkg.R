090205e44451edf0ee718ec1aa93998edbee3794
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-01-16 11:54:00 +0000

    moved scater dependency to Suggests for examples to pass R check

--- R/iSEE-pkg.R
+++ R/iSEE-pkg.R
@@ -16,7 +16,6 @@
 #' renderPrint singleton showModal modalDialog
 #' @importFrom shinydashboard dashboardBody dashboardHeader dashboardPage
 #' dashboardSidebar menuItem tabBox valueBox valueBoxOutput
-#' @importFrom scater plotExpression plotPhenoData
 #' @importFrom utils packageVersion
 #' @importFrom ggplot2 ggplot geom_point labs theme_void aes_string geom_violin
 #'   theme_bw facet_wrap ylab

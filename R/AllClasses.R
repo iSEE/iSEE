ce72656ab95d6b6f7ae3c95e5bba1d90e253049e
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-01-23 22:57:18 +0000

    show method for ExperimentColorMap

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -193,3 +193,31 @@ setMethod("rowDataColorMap", c("ExperimentColorMap", "integer"),
   })
   return(res)
 }
+
+# show ----
+
+setMethod(
+  "show", "ExperimentColorMap",
+  function(object){
+    scat <- function(fmt, vals=character(), exdent=2, ...)
+    {
+        vals <- ifelse(nzchar(vals), vals, "''")
+        lbls <- paste(S4Vectors:::selectSome(vals), collapse=" ")
+        txt <- sprintf(fmt, length(vals), lbls)
+        cat(strwrap(txt, exdent=exdent, ...), sep="\n")
+    }
+
+    cat("Class: ExperimentColorMap\n")
+
+    ## assays()
+    scat("assays(%d): %s\n", names(object@assays))
+
+    ## colData()
+    scat("colData(%d): %s\n", names(object@colData))
+
+    ## rowData()
+    scat("rowData(%d): %s\n", names(object@rowData))
+
+    return(NULL)
+  }
+)

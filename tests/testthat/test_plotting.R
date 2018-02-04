565058d22839eb4cb6ea58ca106b80d71d55b4dd
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-02-04 11:40:22 +0000

    .geneExprsPlot covered for X axis set to column data

--- tests/testthat/test_plotting.R
+++ tests/testthat/test_plotting.R
@@ -178,10 +178,30 @@ test_that(".make_geneExprPlot/.scatter_plot produce a valid list",{
 
 test_that(".make_geneExprPlot works for YAxis set to Gene text", {
   # change the value locally for the specific test
+  selected_gene <- "0610009B22Rik"
+  
   all_memory$geneExpr[1,iSEE:::.geneExprYAxis] <- iSEE:::.geneExprYAxisGeneTextTitle
-  set.seed(42)
-  all_memory$geneExpr[1,iSEE:::.geneExprYAxisGeneText] <- sample(rownames(sce), 1)
+  all_memory$geneExpr[1,iSEE:::.geneExprYAxisGeneText] <- selected_gene
   
   p.out <- iSEE:::.make_geneExprPlot(id = 1, all_memory, all_coordinates, sce, ecm)
+  
+  expect_match(
+    p.out$cmd$data$y,
+    selected_gene
+  )
+  
+})
+
+test_that(".make_geneExprPlot works for XAxis set to Column data", {
+  # change the value locally for the specific test
+  all_memory$geneExpr[1,iSEE:::.geneExprXAxis] <- iSEE:::.geneExprXAxisColDataTitle
+  all_memory$geneExpr[1,iSEE:::.geneExprXAxisColData] <- "dissection_s"
+  
+  p.out <- iSEE:::.make_geneExprPlot(id = 1, all_memory, all_coordinates, sce, ecm)
+  
+  expect_match(
+    p.out$cmd$data$x,
+    "dissection_s"
+  )
   
 })

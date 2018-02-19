187fbe5a20630199f4e93eace24e23fb6b288360
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-02-19 20:52:11 +0000

    gating mode documented and exported

--- R/modes.R
+++ R/modes.R
@@ -1,39 +1,82 @@
 
-mode_CyTOF <- function(
-  se, n_linked = 3,
-  featExprMax = max(5, n_linked),
-  features = sample(rownames(se), size=featExprMax*2, replace=TRUE),
+#' App pre-configured to link multiple feature expression plots
+#'
+#' @param se An object that coercible to
+#'\code{\linkS4class{SingleCellExperiment}}.
+#' @param featExprMax Maximal number of feature expression plots in the app.
+#' @param features \code{data.frame} with columns named \code{x} and \code{y}
+#' that define the features on the axes of the linked plots.
+#' Plots are serially linked from the first row to the last.
+#' @param ... Additional arguments passed to \code{\link{iSEE}}.
+#' @param plot_width The grid width of linked plots (numeric vector of
+#' length either 1 or equal to \code{nrow(features)}
+#' 
+#' @return A Shiny App preconfigure with multiple chain-linked feature
+#' expression plots is launched for interactive data exploration of the
+#' \code{\link{SingleCellExperiment}} / \code{\link{SummarizedExperiment}}
+#' object
+#' 
+#' @export
+#'
+#' @examples
+#' library(scRNAseq)
+#' data(allen)
+#' class(allen)
+#'
+#' # Example data ----
+#'
+#' library(scater)
+#' sce <- as(allen, "SingleCellExperiment")
+#' counts(sce) <- assay(sce, "tophat_counts")
+#' sce <- normalize(sce)
+#' sce <- runPCA(sce)
+#' sce <- runTSNE(sce)
+#' sce
+#'
+#' # launch the app itself ----
+#'
+#' app <- mode_gating(sce, featExprMax = 6)
+#' if (interactive()) {
+#'   shiny::runApp(app, port = 1234)
+#' }
+
+mode_gating <- function(
+  se,
+  featExprMax = 3,
+  features = data.frame(
+    x = sample(rownames(se), size=featExprMax, replace=TRUE),
+    y = sample(rownames(se), size=featExprMax, replace=TRUE)
+  ),
   ..., plot_width = 4){
   # This mode is meaningless with fewer than two featExprPlot
-  stopifnot(n_linked > 1)
+  stopifnot(nrow(features) > 1)
+  stopifnot(all(c("x","y") %in% colnames(features)))
   
   featExprArgs <- featExprPlotDefaults(se, featExprMax)
   # prepare featExprArgs
   featExprArgs[[iSEE:::.featExprXAxis]] <- iSEE:::.featExprXAxisFeatNameTitle
   featExprArgs[[iSEE:::.featExprYAxis]] <- iSEE:::.featExprYAxisFeatNameTitle
   # Y axes take all the odd-numbered feature names
-  featExprArgs[[iSEE:::.featExprXAxisFeatName]] <- features[seq(2,nrow(featExprArgs)*2,2)]
+  featExprArgs[[iSEE:::.featExprXAxisFeatName]] <- features[,"x"]
   # X axes take all the even-numbered feature names
-  featExprArgs[[iSEE:::.featExprYAxisFeatName]] <- features[seq(1,nrow(featExprArgs)*2,2)]
+  featExprArgs[[iSEE:::.featExprYAxisFeatName]] <- features[,"y"]
   featExprArgs[[iSEE:::.brushByPlot]] <- c(
     "",
-    sprintf("Feature expression plot %i", seq(1,n_linked-1,1)),
-    rep("", nrow(featExprArgs) - n_linked)
+    sprintf("Feature expression plot %i", seq(1, nrow(features) - 1, 1)),
+    rep("", nrow(featExprArgs) - nrow(features))
   )
   featExprArgs[[iSEE:::.brushEffect]] <- c(
     "",
-    rep("Restrict", n_linked - 2),
+    rep("Restrict", nrow(features) - 2),
     "Color",
-    rep("", nrow(featExprArgs) - n_linked)
+    rep("", nrow(featExprArgs) - nrow(features))
   )
   # Show only the active 
   initialPanels = DataFrame(
     Name = c(
-      sprintf("Feature expression plot %i", seq(1, n_linked, 1))
+      sprintf("Feature expression plot %i", seq(1, nrow(features), 1))
     ),
-    Width = c(
-      rep(plot_width, n_linked)
-    )
+    Width = plot_width
   )
   # Preconfigure an app
   app <- iSEE(

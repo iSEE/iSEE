dac1e900cc92778ff2900217a46138ea1a221896
Federico Marini <marinif@uni-mainz.de>
2018-02-21 00:04:10 +0100

    Added documentation on internal func .generate_annotation

--- R/annotation.R
+++ R/annotation.R
@@ -1,5 +1,22 @@
+#' Retrieve info on a gene
+#' 
+#' Generate HTML content with more information about a selected gene.
+#'
+#' @param annot.orgdb The OrgDb object
+#' @param annot.keytype The keytype that matches the keys used
+#' @param annot.keyfield The field to use as column from the \code{gene_data}
+#' @param gene_data The data frame containing info on the genes/features of interest,
+#' such as the one generated via \code{as.data.frame(rowData(se))}
+#' @param chosen_gene The selected gene
+#' 
+#' @details This function relies on the code{entrez_summary} function, which 
+#' requires internet access to retrieve updated information. Conversion of the 
+#' ids is handled via OrgDb packages
+#'
+#' @return HTML content with more information about a selected gene
+#' @author Federico Marini
+#' @rdname INTERNAL_generate_annotation
 .generate_annotation <- function(annot.orgdb, annot.keytype, annot.keyfield, gene_data, chosen_gene)
-#  A function to generate a HTML containing more information about a selected gene.
 {
   if (is.null(annot.orgdb)) {
     return(HTML(""))

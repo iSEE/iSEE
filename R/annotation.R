8c0c7176ec783a44489287e437ef8e933180d494
Federico Marini <marinif@uni-mainz.de>
2018-02-23 15:33:18 +0100

    added annotateEntrez and annotateEnsembl as function to address https://github.com/csoneson/iSEE/issues/144

--- R/annotation.R
+++ R/annotation.R
@@ -59,3 +59,123 @@
     )))
   }
 }
+
+
+annotateEntrez <- function(
+  se,
+  species,
+  keytype,
+  rowdata_col = NULL) {
+  
+  function(se, row_index) {
+    # no species provided -> nothing
+    if (is.null(species)) {
+      return(HTML(""))
+    }
+    annopkg <- annoSpecies_df$pkg[annoSpecies_df$species==species]
+    if(!require(annopkg,character.only=TRUE))
+      stop(paste0("The package ",annopkg, " is not installed/available. ",
+                  "Try installing it with biocLite('",annopkg,"')"))
+    
+    # if no column provided, implicitly assume the names are in the rownames of the object itself
+    if (is.null(rowdata_col)) {
+      selectedGene <- rownames(se)[row_index]
+    } else {
+      selectedGene <- rowData(se)[row_index,rowdata_col]
+    }
+    
+    if (keytype!="ENTREZID") {
+      selgene_entrez <- mapIds(eval(parse(text=annopkg)), selectedGene, "ENTREZID", keytype)
+    } else {
+      selgene_entrez <- selectedGene
+    }
+    
+    fullinfo <- entrez_summary("gene", selgene_entrez)
+    link_pubmed <- paste0('<a href="http://www.ncbi.nlm.nih.gov/gene/?term=',
+                          selgene_entrez,
+                          '" target="_blank">Click here to see more at the NCBI database</a>')
+
+    mycontent <- paste0("<b>",fullinfo$name, "</b><br/><br/>",
+                        fullinfo$description,"<br/><br/>",
+                        ifelse(fullinfo$summary == "","",paste0(fullinfo$summary, "<br/><br/>")),
+                        link_pubmed)
+    return(HTML(mycontent))
+  }
+}
+
+annotateEnsembl <- function(
+  se,
+  species,
+  keytype, # will be converted to be/become ENSEMBL
+  rowdata_col = NULL) {
+  
+  function(se, row_index) {
+    # no species provided -> nothing
+    if (is.null(species)) {
+      return(HTML(""))
+    }
+    annopkg <- annoSpecies_df$pkg[annoSpecies_df$species==species]
+    if(!require(annopkg,character.only=TRUE))
+      stop(paste0("The package ",annopkg, " is not installed/available. ",
+                  "Try installing it with biocLite('",annopkg,"')"))
+    
+    # if no column provided, implicitly assume the names are in the rownames of the object itself
+    if (is.null(rowdata_col)) {
+      selectedGene <- rownames(se)[row_index]
+    } else {
+      selectedGene <- rowData(se)[row_index,rowdata_col]
+    }
+    
+    if (keytype!="ENSEMBL") {
+      selgene_ensembl <- mapIds(eval(parse(text=annopkg)), selectedGene, "ENSEMBL", keytype)
+    } else {
+      selgene_ensembl <- selectedGene
+    }
+    
+    link_ensembl <- paste0('<a href="http://www.ensembl.org/',species,'/Gene/Summary?g=',selgene_ensembl,
+                           '" target="_blank"> ',
+                           'Click here to browse more about this gene in the ENSEMBL database</a>')
+    
+    mycontent <- paste0("<b>",selectedGene, "</b><br/><br/>ENSEMBL id:",
+                        selgene_ensembl,"<br/><br/>",
+                        link_ensembl)
+    
+    return(HTML(mycontent))
+  }
+}
+
+
+
+
+# for retrieving the annotation & linking to ensembl? + linking to UCSC afterwards?
+annoSpecies_df <- data.frame(species=c("","Anopheles","Arabidopsis","Bovine","Worm",
+                                       "Canine","Fly","Zebrafish","E coli strain K12",
+                                       "E coli strain Sakai","Chicken","Human","Mouse",
+                                       "Rhesus","Malaria","Chimp","Rat",
+                                       "Yeast","Streptomyces coelicolor", "Pig","Toxoplasma gondii",
+                                       "Xenopus"),
+                             pkg=c("","org.Ag.eg.db", "org.At.tair.db", "org.Bt.eg.db", "org.Ce.eg.db",
+                                   "org.Cf.eg.db", "org.Dm.eg.db", "org.Dr.eg.db", "org.EcK12.eg.db",
+                                   "org.EcSakai.eg.db", "org.Gg.eg.db", "org.Hs.eg.db", "org.Mm.eg.db",
+                                   "org.Mmu.eg.db", "org.Pf.plasmo.db", "org.Pt.eg.db", "org.Rn.eg.db",
+                                   "org.Sc.sgd.db", "org.Sco.eg.db", "org.Ss.eg.db", "org.Tgondii.eg.db",
+                                   "org.Xl.eg.db"),
+                             stringsAsFactors = FALSE)
+annoSpecies_df <- annoSpecies_df[order(annoSpecies_df$species),]
+annoSpecies_df$ensembl_db <- c("","","","Bos_taurus","Canis_familiaris","Gallus_gallus","Pan_troglodytes",
+                               "","","Drosophila_melanogaster","Homo_sapiens","","Mus_musculus",
+                               "Sus_scrofa","Rattus_norvegicus","Macaca_mulatta","","","Caenorhabditis_elegans",
+                               "Xenopus_tropicalis","Saccharomyces_cerevisiae","Danio_rerio")
+rownames(annoSpecies_df) <- annoSpecies_df$species # easier to access afterwards
+
+
+
+
+
+
+
+
+
+
+
+

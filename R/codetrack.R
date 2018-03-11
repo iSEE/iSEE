d62e45389c3b86ded4c235d3af5d62753ad92d55
Aaron Lun <alun@wehi.edu.au>
2018-03-11 14:23:28 +0000

    Fixed code tracker for new selection nomenclature.

--- R/codetrack.R
+++ R/codetrack.R
@@ -3,21 +3,17 @@
 #' Fetches all the code that was used to generate the plots during the live
 #' session of \code{iSEE}
 #' 
-#' @param rObjects A reactive list containing information for all panels (
-#' \code{rObjects$active_panels})
-#' @param pObjects An environment containing \code{memory}, a list of DataFrames
-#' containing parameters for each panel of each type; \code{brush_links}; 
-#' \code{commands} ; \code{extra_plot_cmds}. All of these will be normally 
-#' contained in the environment in use when running \code{iSEE} 
-#' @param se_name The name of the \code{SummarizedExperiment}/\code{SingleCellExperiment}
-#' object
-#' @param ecm_name The name of the \code{ExperimentColorMap} in use
-#' @param sanitize_cmds The character vector containing the commands resulting 
-#' from sanitizing the \code{se} object. This is obtained by extracting \code{cmds}
-#' from the returned output of \code{.sanitize_SE_input}.
+#' @param active_plots A data.frame containing information about the currently active panels, of the same form as that produced by \code{\link{.setup_initial}}.
+#' @param pObjects An environment containing \code{memory}, a list of DataFrames containing parameters for each panel of each type;
+#' \code{selection_links}, a graph object containing point selection links between panels;
+#' \code{commands}, a list of commands required to generated each plot. 
+#' @param se_name String containing the name of the \code{SummarizedExperiment}/\code{SingleCellExperiment} object.
+#' @param ecm_name String containing the name of the \code{ExperimentColorMap} in use.
+#' @param sanitize_cmds Character vector containing the commands used to sanitizing the \code{se} object. 
+#' This is obtained by extracting \code{cmds} from the returned output of \code{.sanitize_SE_input}.
 #'
 #' @return A vector containing the whole code, which can be further passed to 
-#' the instance of the \code{shinyAce} editor in the modal popup of the app
+#' the instance of the \code{shinyAce} editor in the modal popup of the app.
 #' 
 #' @author Federico Marini
 #' @rdname INTERNAL_track_it_all
@@ -26,7 +22,7 @@
   # Commands only reported for plots, not for the tables
   aobjs <- as.data.frame(rObjects$active_panels)
   aobjs <- aobjs[aobjs$Type!="rowStatTable" & aobjs$Type!="heatMapPlot",]
-  aobjs <- aobjs[.get_reporting_order(aobjs, pObjects$brush_links),]
+  aobjs <- aobjs[.get_reporting_order(aobjs, pObjects$selection_links),]
 
   # storing to a text character vector
   tracked_code <- c(
@@ -93,11 +89,11 @@
     # Adding the plotting commands.
     cur_cmds <- pObjects$commands[[panel_name]]
     collated <- c(cur_cmds$data, "")
-    if (length(cur_cmds$brush)) {
-        collated <- c(collated, "# Receiving brush data", cur_cmds$brush, "")
+    if (length(cur_cmds$select)) {
+        collated <- c(collated, "# Receiving point selection", cur_cmds$select, "")
     }
 
-    # Saving data for transmission after brushes have been processed;
+    # Saving data for transmission after selections have been processed;
     # this is the equivalent point in .create_plots() where coordinates are saved.
     collated <- c(collated, "# Saving data for transmission", 
                   sprintf("all_coordinates[['%s']] <- plot.data", panel_name),
@@ -132,8 +128,8 @@
     collated <- c(cur_cmds$data, "")
 
     # Finishing off the rest of the commands.
-    if (length(cur_cmds$brush)) { 
-        collated <- c(collated, "# Receiving brush data", cur_cmds$brush, "")
+    if (length(cur_cmds$select)) { 
+        collated <- c(collated, "# Receiving selection data", cur_cmds$select, "")
     }
     if (length(cur_cmds$zoom)) { 
         collated <- c(collated, "# Zooming in", cur_cmds$zoom, "")
@@ -172,48 +168,64 @@
 
 #' Get the reporting order
 #'
-#' Reordering plots by whether or not they exhibited brushing.
+#' Reordering plots by whether or not they transmit point selection information.
 #' 
-#' @param active_plots The active plots, obtained for example after calling
-#' \code{as.data.frame(rObjects$active_panels)}
-#' @param brush_chart The links between brushed objects (e.g. via 
-#' \code{pObjects$brush_links})
+#' @param active_plots A data.frame containing information about the currently active panels, of the same form as that produced by \code{\link{.setup_initial}}.
+#' @param select_chart A graph object containing information about how point selection information is transferred between panels.
 #'
-#' @return Indices for the plots, sorted upon active usage of brushing or not
-#' @author Federico Marini
+#' @details
+#' This function is necessary to ensure that coordinates in transmitting plots are generated before point selection is performed in receiving plots.
+#'
+#' @return An integer vector specifying the order of the panels for reporting code.
+#' @author Aaron Lun
 #' @rdname INTERNAL_get_reporting_order
 #' @importFrom igraph topo_sort
-.get_reporting_order <- function(active_plots, brush_chart) 
+.get_reporting_order <- function(active_plots, select_chart) 
 {
   N <- nrow(active_plots)
   node_names <- sprintf("%s%i", active_plots$Type, active_plots$ID)
-  ordering <- topo_sort(brush_chart, "out")
+  ordering <- topo_sort(select_chart, "out")
   order(match(node_names, names(ordering)))
 }
 
 
 #' Plot a snapshot of the links among panels
 #'
-#' Reads in the stored reactive (\code{rObjects}) and the \code{pObjects}
-#' environment, and creates a graph of the current links among all panels 
-#' open in the app 
-#' @param rObjects A reactive list containing incrementable counters for all panels
-#' @param pObjects An environment containing \code{pObjects$brush_links}
+#' Reads in the stored reactive (\code{rObjects}) and the \code{pObjects} environment, and creates a graph of the current links among all panels open in the app.
+#'
+#' @param active_plots A data.frame containing information about the currently active plots.
+#' @param rObjects A reactive list containing incrementable counters for all panels.
+#' @param pObjects An environment containing \code{pObjects$selection_links}.
 #'
 #' @return Plots the graph representing the snapshot of links among plots/panels
 #' @author Federico Marini
 #' @rdname INTERNAL_snapshot_graph_linkedpanels
-#' @importFrom igraph delete.vertices set_vertex_attr V
-.snapshot_graph_linkedpanels <- function(rObjects, pObjects) 
-{
+#' @importFrom igraph delete.vertices set_vertex_attr V add_edges
+.snapshot_graph_linkedpanels <- function(rObjects, pObjects) {
   cur_plots <- sprintf("%s%i", rObjects$active_panels$Type, rObjects$active_panels$ID)
-  not_used <- setdiff(V(pObjects$brush_links)$name,cur_plots)
-  currgraph_used <- delete.vertices(pObjects$brush_links,not_used)
-  currgraph_used <- set_vertex_attr(currgraph_used,"plottype",
-                                    value = sub("[0-9]+$","",V(currgraph_used)$name))
+  not_used <- setdiff(V(pObjects$selection_links)$name,cur_plots)
+  currgraph <- delete.vertices(pObjects$selection_links, not_used)
+  currgraph <- set_vertex_attr(currgraph,"plottype", value = sub("[0-9]+$","",V(currgraph)$name))
   
-  currgraph_used <- .find_links_to_table(rObjects, pObjects, currgraph_used)
+  # Incorporating table links as well.
+  tbl_objs <- rObjects$active_panels[rObjects$active_panels$Type=="rowStatTable",]
+  cur_tables <- sprintf("%s%i",tbl_objs$Type,tbl_objs$ID)
+  all_tlinks <- pObjects$table_links
+  cur_tlinks <- all_tlinks[cur_tables]
   
+  for (i in seq_len(nrow(tbl_objs))) {
+    table_used <- cur_tables[i]
+    col_links <- unlist(cur_tlinks[[table_used]]$color)
+    x_links <- unlist(cur_tlinks[[table_used]]$xaxis)
+    y_links <- unlist(cur_tlinks[[table_used]]$yaxis)
+    any_links <- unique(c(col_links,x_links,y_links))
+    
+    for(j in seq_len(length(any_links))){
+      curgraph <- add_edges(curgraph, c(table_used, any_links[j]))
+    }
+  }
+ 
+  # Creating the plot. 
   plot(currgraph_used,
        edge.arrow.size = .8,
        vertex.label.cex = 1.3,
@@ -223,40 +235,3 @@
        vertex.color = panel_colors[V(currgraph_used)$plottype])
 }  
 
-
-#' Find links in the table used
-#' 
-#' Finds the links for the tables that are used, and sets the new edges in the graph
-#' this updated graph is then returned as output
-#'
-#' @param rObjects A reactive list containing info on the active panels
-#' @param pObjects An environment containing \code{table_links}, a list of DataFrames containing parameters for each table
-#' @param graph The graph object, generated for example with \code{pObjects$brush_links}, 
-#' after pruning of the unused vertices
-#'
-#' @return The updated graph
-#' @author Federico Marini
-#' @rdname INTERNAL_find_links_to_table
-#' @importFrom igraph add_edges
-.find_links_to_table <- function(rObjects, pObjects, graph)
-{
-  tbl_objs <- rObjects$active_panels[rObjects$active_panels$Type=="rowStatTable",]
-  cur_tables <- sprintf("%s%i",tbl_objs$Type,tbl_objs$ID)
-  all_tlinks <- pObjects$table_links
-  cur_tlinks <- all_tlinks[cur_tables]
-  
-  # for every table in use
-  for (i in seq_len(nrow(tbl_objs))) {
-    table_used <- cur_tables[i]
-    col_links <- unlist(cur_tlinks[[table_used]]$color)
-    x_links <- unlist(cur_tlinks[[table_used]]$xaxis)
-    y_links <- unlist(cur_tlinks[[table_used]]$yaxis)
-    any_links <- unique(c(col_links,x_links,y_links))
-    
-    # add the edges corresponding
-    for(j in seq_len(length(any_links))){
-      graph <- add_edges(graph,c(table_used, any_links[j]))
-    }
-  }
-  return(graph)
-}

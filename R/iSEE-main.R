058a651fd8e45276f2336cc1259b5423fcfc0bbf
Aaron Lun <alun@wehi.edu.au>
2018-03-17 17:21:28 +0000

    Added rowData to ?iSEE example.

--- R/iSEE-main.R
+++ R/iSEE-main.R
@@ -88,8 +88,11 @@
 #' sce <- as(allen, "SingleCellExperiment")
 #' counts(sce) <- assay(sce, "tophat_counts")
 #' sce <- normalize(sce)
+#'
 #' sce <- runPCA(sce)
 #' sce <- runTSNE(sce)
+#' rowData(sce)$ave_count <- rowMeans(counts(sce))
+#' rowData(sce)$n_cells <- rowSums(counts(sce)>0)
 #' sce
 #'
 #' # launch the app itself ----

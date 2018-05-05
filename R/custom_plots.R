957e3765cf623afd70907c886f6bde705c3ff400
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-05-05 18:43:04 +0100

    Bug fixes to custom plot function, documented custom fun requirements.

--- R/custom_plots.R
+++ R/custom_plots.R
@@ -67,10 +67,11 @@
     eval_env <- new.env()
     eval_env$se <- se
     eval_env$all_coordinates <- all_coordinates
+    eval_env$custom_col_fun <- .get_custom_col_fun(se)
 
     # Mocking up a plot.data.
-    data_cmds <- character(0)
-    data_cmds[["init"]] <- c("plot.data <- data.frame(row.names=colnames(se));")
+    data_cmds <- list()
+    data_cmds[["init"]] <- "plot.data <- data.frame(row.names=colnames(se));"
 
     # Adding coloring information as well.    
     color_out <- .define_colorby_for_column_plot(param_choices, se)
@@ -78,18 +79,23 @@
     color_lab <- color_out$label
 
     # Ensuring that colors are either factor or numeric. 
+    data_cmds <- unlist(data_cmds)
     eval(parse(text=data_cmds), envir=eval_env)
 
     coloring <- eval_env$plot.data$ColorBy
     if (!is.null(coloring)) {
         color_coerce_cmd <- .coerce_type(coloring, "ColorBy", as_numeric=!.is_groupable(coloring))
-        eval(parse(text=color_coerce_cmd), envir=eval_env)
-        data_cmds <- c(data_cmds, color_coerce_cmd)
+        if (!is.null(color_coerce_cmd)) { 
+            eval(parse(text=color_coerce_cmd), envir=eval_env)
+            data_cmds <- c(data_cmds, color_coerce_cmd)
+        }
     }
 
-    # Implementing the selection. 
+    # Implementing the selection, and _eliminating_ plot.data.all.
+    # This is not necessary when plotting only the restricted subset.
     select_out <- .process_selectby_choice(param_choices, all_memory)
     select_cmds <- select_out$cmds
+    select_cmds <- select_cmds[!grepl("^plot.data.all", select_cmds)]
     if (length(select_cmds)) { 
         eval_env$all_brushes <- select_out$data
         eval_env$all_lassos <- select_out$data
@@ -99,20 +105,25 @@
     # Constructing the evaluation commands to get the points.
     # Checking whether the selected points are the same as before.
     # Otherwise we 'fill in' the cached results.
-    eval_env$all_functions <- .get_custom_col_fun(se)
-    enerator <- sprintf("custom.data <- all_functions[[%s]](se, rownames(plot.data));", deparse(param_choices[[.customColFun]]))
+    validate(need(
+        param_choices[[.customColFun]]!=.noSelection,
+        "No function selected"
+    ))
+    generator <- sprintf("custom.data <- custom_col_fun[[%s]](se, rownames(plot.data));", deparse(param_choices[[.customColFun]]))
 
-    if (!identical(rownames(eval_env$plot.data), rownames(cached))) {
+    if (!identical(rownames(eval_env$plot.data), rownames(cached$coordinates))) {
         eval(parse(text=generator), envir=eval_env)
         cached <- eval_env$custom.data 
     } else {
         eval_env$custom.data <- cached
     }
 
-    # Coercing type of the X and Y values.
-    custom_cmds <- character(0)
-    custom_cmds[["reset"]] <- c("plot.data$X <- custom.data$X;", "plot.data$Y <- custom.data$Y;")
+    assigner <- c("plot.data$X <- custom.data$coordinates$X;", "plot.data$Y <- custom.data$coordinates$Y;")
+    eval(parse(text=assigner), envir=eval_env)
+    generator <- c(generator, assigner)
 
+    # Coercing type of the X and Y values.
+    custom_cmds <- list()
     xvals <- eval_env$plot.data$X
     group_X <- .is_groupable(xvals)
     custom_cmds[["more_X"]] <- .coerce_type(xvals, "X", as_numeric=!group_X)
@@ -121,7 +132,10 @@
     group_Y <- .is_groupable(yvals)
     custom_cmds[["more_Y"]] <- .coerce_type(yvals, "Y", as_numeric=!group_Y)
 
-    eval(parse(text=custom_cmds), envir=eval_env)
+    custom_cmds <- unlist(custom_cmds)
+    if (length(custom_cmds)) {
+        eval(parse(text=custom_cmds), envir=eval_env)
+    }
     custom_cmds <- c(generator, custom_cmds)
 
     # Adding more plot-specific information, depending on the type of plot to be created.
@@ -136,7 +150,40 @@
     xlab <- cached$xlab
     ylab <- cached$ylab
     plot_title <- cached$title
-    plot_out <- .create_plot(eval_env, param_choices, x_lab=xlab, y_lab=ylab, title=plot_title, color_lab = color_lab, by_row = FALSE)
+    plot_out <- .create_plot(eval_env, param_choices, colormap=colormap, x_lab=xlab, y_lab=ylab, title=plot_title, color_lab = color_lab, by_row = FALSE)
 
     return(list(cmd_list = c(cmd_list, list(plot=c(downsample_cmds, plot_out$cmds))), xy = xy, plot = plot_out$plot, cached = cached))
 }
+
+#' @name Custom iSEE plots
+#' @title Creating custom plots
+#' @description Instructions for creating custom plots for use in \code{\link{iSEE}}.
+#'
+#' @section Functions for custom column plots:
+#' Any function should accept the following arguments:
+#' \itemize{
+#' \item \code{se}, a SingleCellExperiment object.
+#' \item \code{columns}, a character vector of column names, corresponding to a transmitted selection of points.
+#' This will be different from \code{colnames(se)} if the current custom column plot is set to \code{"Restrict"}.
+#' }
+#' In return, it should produce a list containing the following elements:
+#' \describe{
+#' \item{\code{coordinates}:}{A data.frame with one row per entry of \code{columns}.
+#' It should contain the fields \code{X} and \code{Y}, for the x- and y-axis values per row.
+#' Row names must be set to \code{columns}.}
+#' \item{\code{xlab}:}{A string specifying the x-axis label.}
+#' \item{\code{ylab}:}{A string specifying the y-axis label.}
+#' \item{\code{title}:}{A string specifying the plot title.}
+#' }
+#'
+#' @author Aaron Lun
+#' @rdname custom_plots
+#' @examples
+#' CUSTOM <- function(se, columns) {
+#'     kept <- logcounts(se)[,columns]
+#'     out <- prcomp(t(kept), rank.=2)$x
+#'     coords <- data.frame(X=out[,1], Y=out[,2], row.names=columns)
+#'     return(list(coordinates=coords, xlab="PC1", ylab="PC2", 
+#'            title="PCA on selected points"))           
+#' }
+NULL

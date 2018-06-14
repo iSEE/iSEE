4dd4a0f5164668b07e6e63f6a199515c923b2506
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-06-14 22:21:04 +0100

    fix facet related UI tests

--- tests/testthat/test_ui.R
+++ tests/testthat/test_ui.R
@@ -3,41 +3,56 @@
 test_that(".add_facet_UI_elements produces a valid tag list for column data plots", {
     
     rd <- redDimPlotDefaults(sce, 1)
+    rd[["FacetByRow"]] <- TRUE
+    rd[["FacetByColumn"]] <- TRUE
   
-    # Default is '.' and '.' selected for row and column faceting, respectively
-    out <- iSEE:::.add_facet_UI_elements("redDimPlot", 1, rd, colnames(colData(sce)))
-    # Row
-    expect_match(
-        out[[1]]$children[[2]]$children[[1]]$attribs[[1]],
-        "RowFacetColData"
+    out <- iSEE:::.add_facet_UI_elements_for_column_plots("redDimPlot", 1, rd, colnames(colData(sce)))
+    
+    # Row checkbox is ticked
+    expect_identical(
+        out[[1]]$children[[1]]$children[[1]]$children[[1]]$attribs$id,
+        "redDimPlot1_FacetByRow"
     )
-    expect_match(
-        out[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
-        '<option value=\".\" selected>', fixed = TRUE
+    expect_identical(
+        out[[1]]$children[[1]]$children[[1]]$children[[1]]$attribs$checked,
+        "checked"
     )
-    # Column
+    # Default covariate is selected
     expect_match(
-        out[[2]]$children[[2]]$children[[1]]$attribs[[1]],
-        "ColumnFacetColData"
+        out[[2]]$children[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
+        "<option value=\"NREADS\" selected>", fixed = TRUE
     )
+
+    # Column checkbox is ticked
+    expect_identical(
+        out[[3]]$children[[1]]$children[[1]]$children[[1]]$attribs$id,
+        "redDimPlot1_FacetByColumn"
+    )
+    expect_identical(
+        out[[3]]$children[[1]]$children[[1]]$children[[1]]$attribs$checked,
+        "checked"
+    )
+    # Default covariate is selected
     expect_match(
-        out[[2]]$children[[2]]$children[[1]]$children[[1]][[1]],
-        '<option value=\".\" selected>', fixed = TRUE
+        out[[4]]$children[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
+        "<option value=\"NREADS\" selected>", fixed = TRUE
     )
     
     # Check a non default choice
     
     rd[["RowFacetColData"]] <- "driver_1_s"
     rd[["ColumnFacetColData"]] <- "NREADS"
-    out <- iSEE:::.add_facet_UI_elements("redDimPlot", 1, rd, colnames(colData(sce)))
+    out <- iSEE:::.add_facet_UI_elements_for_column_plots("redDimPlot", 1, rd, colnames(colData(sce)))
+    
     # Row facet
     expect_match(
-        out[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
+        out[[2]]$children[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
         '<option value=\"driver_1_s\" selected>', fixed = TRUE
     )
+    
     # Column facet
     expect_match(
-        out[[2]]$children[[2]]$children[[1]]$children[[1]][[1]],
+        out[[4]]$children[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
         '<option value=\"NREADS\" selected>', fixed = TRUE
     )
 
@@ -46,43 +61,56 @@ test_that(".add_facet_UI_elements produces a valid tag list for column data plot
 test_that(".add_facet_UI_elements produces a valid tag list for row data plots", {
     
     rd <- rowDataPlotDefaults(sce, 1)
+    rd[["FacetByRow"]] <- TRUE
+    rd[["FacetByColumn"]] <- TRUE
   
     # Default is '.' and '.' selected for row and column faceting, respectively
     
-    out <- iSEE:::.add_facet_UI_elements("rowDataPlot", 1, rd, colnames(rowData(sce)))
+    out <- iSEE:::.add_facet_UI_elements_for_row_plots("rowDataPlot", 1, rd, colnames(rowData(sce)))
+    
     # Row
-    expect_match(
-        out[[1]]$children[[2]]$children[[1]]$attribs[[1]],
-        "RowFacetColData"
+    expect_identical(
+        out[[1]]$children[[1]]$children[[1]]$children[[1]]$attribs$id,
+        "rowDataPlot1_FacetByRow"
     )
-    expect_match(
-        out[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
-        '<option value=\".\" selected>', fixed = TRUE
+    expect_identical(
+        out[[1]]$children[[1]]$children[[1]]$children[[1]]$attribs$checked,
+        "checked"
     )
+    # Default covariate is selected
+    expect_match(
+        out[[2]]$children[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
+        "<option value=\"num_cells\" selected>", fixed = TRUE
+    )
+    
     # Column
-    expect_match(
-        out[[2]]$children[[2]]$children[[1]]$attribs[[1]],
-        "ColumnFacetColData"
+    expect_identical(
+        out[[3]]$children[[1]]$children[[1]]$children[[1]]$attribs$id,
+        "rowDataPlot1_FacetByColumn"
+    )
+    expect_identical(
+        out[[3]]$children[[1]]$children[[1]]$children[[1]]$attribs$checked,
+        "checked"
     )
     expect_match(
-        out[[2]]$children[[2]]$children[[1]]$children[[1]][[1]],
-        '<option value=\".\" selected>', fixed = TRUE
+        out[[4]]$children[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
+        "<option value=\"num_cells\" selected>", fixed = TRUE
     )
     
     # Check a non default choice
     
-    rd[["RowFacetColData"]] <- "mean_count"
-    rd[["ColumnFacetColData"]] <- "num_cells"
-    out <- iSEE:::.add_facet_UI_elements("rowDataPlot", 1, rd, colnames(rowData(sce)))
+    rd[["RowFacetRowData"]] <- "mean_count"
+    rd[["ColumnFacetRowData"]] <- "mean_count"
+    out <- iSEE:::.add_facet_UI_elements_for_row_plots("rowDataPlot", 1, rd, colnames(rowData(sce)))
     # Row facet
     expect_match(
-        out[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
+        out[[2]]$children[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
         '<option value=\"mean_count\" selected>', fixed = TRUE
     )
     # Column facet
     expect_match(
-        out[[2]]$children[[2]]$children[[1]]$children[[1]][[1]],
-        '<option value=\"num_cells\" selected>', fixed = TRUE
+        out[[4]]$children[[1]]$children[[2]]$children[[1]]$children[[1]][[1]],
+        '<option value=\"mean_count\" selected>', fixed = TRUE
     )
 
 })

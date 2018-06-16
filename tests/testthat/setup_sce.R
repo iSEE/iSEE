0764c739e4d26bc5ae3b91bd5aa4054e8a50f774
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-06-16 17:06:42 +0100

    test .panel_generation

--- tests/testthat/setup_sce.R
+++ tests/testthat/setup_sce.R
@@ -14,6 +14,8 @@ sce <- runTSNE(sce)
 
 rowData(sce)$num_cells <- rowSums(counts(sce)>0)
 rowData(sce)$mean_count <- rowMeans(counts(sce))
+# Add a groupable field in rowData
+rowData(sce)$letters <- sample(letters[1:3], nrow(sce), TRUE)
 
 isSpike(sce, "random") <- sample(c(TRUE, FALSE), nrow(sce), replace = TRUE, prob = c(0.01, 0.99))
 

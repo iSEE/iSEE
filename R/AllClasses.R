549dd30a3c0215314318b80eb801784f2d8aeebc
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-07-15 23:46:23 +0100

    replace sapply by lapply

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -337,10 +337,13 @@ ExperimentColorMap <- function(
     if (is.null(names(usr))){
         stop("User-defined color map must be a named list")
     }
-    usr <- sapply(usr, .substituteNullColorMap)
+    def_names <- names(def)
+    usr <- usr[def_names]
+    usr <- lapply(usr, .substituteNullColorMap)
+    names(usr) <- def_names
     # set the color maps given by the user
     # note that invalid ones will be picked up by the class validity check later
-    def[names(usr)] <- usr
+    def <- usr
     return(def)
 }
 

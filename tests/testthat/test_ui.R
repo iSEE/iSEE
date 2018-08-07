ecebaf0580d0c5f0fcff63ac0ea9986acd2bfdbc
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-08-07 23:01:24 +0100

    new custom names in test_ui

--- tests/testthat/test_ui.R
+++ tests/testthat/test_ui.R
@@ -7,15 +7,27 @@ CUSTOM <- function(se, columns) {
                 xlab="WHEE", ylab="YAY", title="HOORAY"))
 }
 
+CUSTOM_DE <- function(se, columns) {
+    stuff <- logcounts(se)[ ,columns]
+    # Completely ignore stuff and return depressing DE stats
+    out <- data.frame(
+        FC=rep(0, nrow(se)),
+        p.value=rep(1, nrow(se)),
+        row.names=rownames(se)
+    )
+    return(out)
+}
+
 initialPanels <- DataFrame(
     Name = paste(c(
         "Reduced dimension plot",
         "Column data plot",
         "Feature assay plot",
-        "Sample assay plot", 
+        "Sample assay plot",
         "Row statistics table",
         "Row data plot",
-        "Custom column plot",
+        "Custom data plot",
+        "Custom statistics table",
         "Heat map"),
         1),
     Width = 3
@@ -25,86 +37,90 @@ initialPanels <- DataFrame(
 redDimArgs <- redDimPlotDefaults(sce, 1)
 colDataArgs <- colDataPlotDefaults(sce, 1)
 featAssayArgs <- featAssayPlotDefaults(sce, 1)
-sampAssayArgs <- sampAssayPlotDefaults(sce, 1)
 rowStatArgs <- rowStatTableDefaults(sce, 1)
 rowDataArgs <- rowDataPlotDefaults(sce, 1)
-customColArgs <- customDataPlotDefaults(sce, 1)
+sampAssayArgs <- sampAssayPlotDefaults(sce, 1)
+customDataArgs <- customDataPlotDefaults(sce, 1)
+customStatArgs <- customStatTableDefaults(sce, 1)
 heatMapArgs <- heatMapPlotDefaults(sce, 1)
-customColArgs <- customDataPlotDefaults(sce, 1)
 
 #
-customColArgs$Function <- "PCA2"
+customDataArgs$Function <- "PCA2"
+customStatArgs$Function <- "DE"
 
 # Adding row names to mimic .setup_memory().
-# We don't actually want to run that function, though, 
-# as the number of customColPlots will be set to zero. 
+# We don't actually want to run that function, though,
+# as the number of customColPlots will be set to zero.
 rownames(redDimArgs) <- sprintf("redDimPlot%i", seq_len(nrow(redDimArgs)))
 rownames(colDataArgs) <- sprintf("colDataPlot%i", seq_len(nrow(colDataArgs)))
 rownames(featAssayArgs) <- sprintf("featAssayPlot%i", seq_len(nrow(featAssayArgs)))
 rownames(sampAssayArgs) <- sprintf("sampAssayPlot%i", seq_len(nrow(sampAssayArgs)))
 rownames(rowStatArgs) <- sprintf("rowStatTable%i", seq_len(nrow(rowStatArgs)))
 rownames(rowDataArgs) <- sprintf("rowDataPlot%i", seq_len(nrow(rowDataArgs)))
-rownames(customColArgs) <- sprintf("customColPlot%i", seq_len(nrow(customColArgs)))
+rownames(customDataArgs) <- sprintf("customDataPlot%i", seq_len(nrow(customDataArgs)))
+rownames(customStatArgs) <- sprintf("customStatTable%i", seq_len(nrow(customStatArgs)))
 rownames(heatMapArgs) <- sprintf("heatMapPlot%i", seq_len(nrow(heatMapArgs)))
 
 # Setting up the memory.
-memory <- list(redDimPlot=redDimArgs, 
-               colDataPlot=colDataArgs, 
-               featAssayPlot=featAssayArgs, 
-               sampAssayPlot=sampAssayArgs,
-               rowStatTable=rowStatArgs, 
-               rowDataPlot=rowDataArgs, 
-               customColPlot=customColArgs,
-               heatMapPlot=heatMapArgs)
+memory <- list(
+    redDimPlot=redDimArgs,
+    colDataPlot=colDataArgs,
+    featAssayPlot=featAssayArgs,
+    sampAssayPlot=sampAssayArgs,
+    rowStatTable=rowStatArgs,
+    rowDataPlot=rowDataArgs,
+    customDataPlot=customDataArgs,
+    customStatTable=customStatArgs,
+    heatMapPlot=heatMapArgs)
 
 # Set up alternative object.
-sceX <- iSEE:::.precompute_UI_info(sce, list(PCA2=CUSTOM))
+sceX <- iSEE:::.precompute_UI_info(sce, list(PCA2=CUSTOM), list(DE=CUSTOM_DE))
 active_panels <- iSEE:::.setup_initial(initialPanels, memory)
 memory <- iSEE:::.sanitize_memory(active_panels, memory)
-    
+
 # .add_coldata_facet_UI_elements ----
 
 test_that(".add_facet_UI_elements produces a valid tag list for column data plots", {
-    
+
     rd <- redDimPlotDefaults(sce, 1)
     rd[["FacetByRow"]] <- TRUE
     rd[["FacetByColumn"]] <- TRUE
-    
+
     groupable_colData <- colnames(colData(sce))[iSEE:::.which_groupable(colData(sce))]
-  
+
     out <- iSEE:::.add_facet_UI_elements_for_column_plots("redDimPlot", 1, rd, groupable_colData)
     # TODO: expect_*(out)
-    
+
     # Check a non default choice
     rd[["RowFacetColData"]] <- "Primary.Type"
     rd[["ColumnFacetColData"]] <- "Core.Type"
     out <- iSEE:::.add_facet_UI_elements_for_column_plots("redDimPlot", 1, rd, groupable_colData)
-    
+
     # TODO: expect_*(out)
 })
 
 test_that(".add_facet_UI_elements produces a valid tag list for row data plots", {
-    
+
     rd <- rowDataPlotDefaults(sce, 1)
     rd[["FacetByRow"]] <- TRUE
     rd[["FacetByColumn"]] <- TRUE
-    
+
     groupable_colData <- colnames(colData(sce))[iSEE:::.which_groupable(rowData(sce))]
-    
+
     out <- iSEE:::.add_facet_UI_elements_for_row_plots("rowDataPlot", 1, rd, groupable_colData)
     # TODO: expect_*(out)
 })
 
 test_that(".panel_generation works", {
-    
+
     out <- iSEE:::.panel_generation(active_panels, memory, sceX)
-    
+
     expect_is(out, "shiny.tag.list")
 })
 
 test_that(".panel_organization works", {
-    
+
     out <- iSEE:::.panel_organization(active_panels)
-    
+
     expect_is(out, "shiny.tag.list")
 })

07fb7ef68f43e176de9d8ec1425a48e1dc9835c1
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-07 10:05:29 +0100

    Bugfix to grouping constants.

--- R/constants.R
+++ R/constants.R
@@ -216,7 +216,7 @@ names(rev.translation) <- translation
 # Refer to dynamicUI.R for the minimum expected structure for each group.
 row_point_plot_types <- c("rowDataPlot", "sampAssayPlot")
 col_point_plot_types <- c("redDimPlot", "colDataPlot", "featAssayPlot")
-point_plot_types <- c(col_point_plot_types, row_point_row_types)
+point_plot_types <- c(col_point_plot_types, row_point_plot_types)
 linked_table_types <- c("rowStatTable")
 custom_panel_types <- c("customDataPlot", "customStatTable")
 all_panel_types <- c(point_plot_types, table_types, custom_panel_types, "heatMapPlot")

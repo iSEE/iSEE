86ca0d7df6f2b558aad1186e11771d0d20a66185
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-08-16 20:06:21 +0100

    fix modeGating and unit test to deal with new arguments

--- tests/testthat/test_modes.R
+++ tests/testthat/test_modes.R
@@ -5,27 +5,27 @@
 
 # .make_redDimPlot/.scatter_plot ----
 
-test_that(".make_redDimPlot/.scatter_plot produce a valid list",{
-  
+test_that("modeGating returns a Shiny app",{
+
   plot_count <- 6
-  rv <- rowVars(logcounts(sce))
-  top_var <- head(order(rv, decreasing = TRUE), plot_count*2)
-  top_var_genes <- rownames(sce)[top_var]
-  
+  # rv <- rowVars(logcounts(sce))
+  # top_var <- head(order(rv, decreasing = TRUE), plot_count*2)
+  # top_var_genes <- rownames(sce)[top_var]
+
   plot_features <- data.frame(
-      x = head(top_var_genes, plot_count),
-      y = tail(top_var_genes, plot_count),
+      x = head(rownames(sce), plot_count),
+      y = tail(rownames(sce), plot_count),
       stringsAsFactors = FALSE
    )
-  
+
   # launch the app itself ----
-  
+
   app <- modeGating(sce, features = plot_features, featAssayMax = 12)
-    
+
   # return value is a named list
   expect_s3_class(
     app,
     "shiny.appobj"
   )
-  
+
 })

b86b3064a63657257a45075488f6884d2124d5d2
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-09-28 19:02:15 +0100

    devtools still somehow points to BiocInstaller; install from bioc-devel instead

--- inst/extdata/install_iSEE.R
+++ inst/extdata/install_iSEE.R
@@ -1,5 +1,5 @@
 if (!require("BiocManager"))
     install.packages("BiocManager")
-devtools::install(build_vignettes=TRUE)
+BiocManager::install("iSEE", version="devel")
 BiocManager::install("org.Hs.eg.db") # required for the PBMC4k demo
 BiocManager::install("HDF5Array") # required for HDF5-backed assays

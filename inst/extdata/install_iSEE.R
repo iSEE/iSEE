1e1856f9390505d21297141d6a2aaadbebae45ab
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-09-28 10:39:57 +0100

    remove biocLite again; wait for DelayedArray to make it to the devel branch

--- inst/extdata/install_iSEE.R
+++ inst/extdata/install_iSEE.R
@@ -1,7 +1,5 @@
-source("http://bioconductor.org/biocLite.R") # require to install DelayedArray below
 if (!require("BiocManager"))
     install.packages("BiocManager")
 devtools::install(build_vignettes=TRUE)
 BiocManager::install("org.Hs.eg.db") # required for the PBMC4k demo
 BiocManager::install("HDF5Array") # required for HDF5-backed assays
-devtools::install_github("Bioconductor/DelayedArray") # version 0.7.44 required

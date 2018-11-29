e284dfdfeed118b890f7ca7015c7af837d629085
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-11-29 19:19:48 +0000

    voice vignette; update readme; simpler voice command definitions

--- inst/www/voice.js
+++ inst/www/voice.js
@@ -14,23 +14,23 @@ var initVoice = function() {
         var showActivePanelCount = 0;
         var goodBoyCount = 0;
         var commands = {
-            'show (me) (us) panel *panel': function(panel) {
+            'show(n) panel *panel': function(panel) {
                 Shiny.onInputChange(voiceShowPanelInput, panel);
             },
             'hide panel *panel': function(panel) {
                 Shiny.onInputChange(voiceHidePanelInput, panel);
             },
-            '(give me) control (of) (the) panel *panel': function(panel) {
+            'control panel *panel': function(panel) {
                 Shiny.onInputChange(voiceControlPanelInput, panel);
             },
-            'show (me) (us) (the) active panel': function() {
+            'show active panel': function() {
                 showActivePanelCount++;
                 Shiny.onInputChange(voiceShowActivePanelInput, showActivePanelCount);
             },
-            'colo(u)r using *choice': function(choice) {
+            'colour using *choice': function(choice) {
                 Shiny.onInputChange(voiceColorUsingInput, choice);
             },
-            'colo(u)r by *choice': function(choice) {
+            'colour by *choice': function(choice) {
                 Shiny.onInputChange(voiceColorByInput, choice);
             },
             'receive(d) (selection) from (panel) *panel': function(panel) {

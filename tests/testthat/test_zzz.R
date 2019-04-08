aa3acb79b693efa17cf9555631c86d4b02edbe7a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-04-08 16:36:34 -0700

    Correct resource path for the time being to fix #284.

--- tests/testthat/test_zzz.R
+++ tests/testthat/test_zzz.R
@@ -4,7 +4,7 @@ test_that("onLoad works", {
 
     iSEE:::.onLoad()
 
-    directoryPath <- normalizePath(shiny:::.globals$resources[["iSEE"]][["directoryPath"]])
+    directoryPath <- normalizePath(shiny:::.globals$resourcePaths[["iSEE"]][["path"]])
     expectedPath <- normalizePath(system.file("www", package="iSEE"))
     # expect_identical(directoryPath, expectedPath)
 

ccfa9992786fd40b93e4e6d03e11bd2aa325f352
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2019-07-17 22:53:54 +0100

    typo

--- tests/testthat/test_groupable.R
+++ tests/testthat/test_groupable.R
@@ -2,7 +2,7 @@ context("groupable")
 
 # .which_numeric ----
 
-test_that(".which_numeric si robust in the absence of columns", {
+test_that(".which_numeric is robust in the absence of columns", {
 
     out <- .which_numeric(DataFrame())
     expect_identical(out, integer(0L))

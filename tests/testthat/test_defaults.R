792397cb9e7b80a4f32f08c65232a8775adecad9
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2019-08-17 11:44:23 +0100

    fix behavior to support absence of any reducedDim

--- tests/testthat/test_defaults.R
+++ tests/testthat/test_defaults.R
@@ -1,67 +1,87 @@
 context("defaults")
 
+# redDimPlotDefaults ----
+
+test_that("redDimPlotDefaults supports objects without reduced dimensions", {
+    sce0 <- SingleCellExperiment()
+
+    out <- redDimPlotDefaults(sce0, 1)
+    expect_identical(out$Type, NA_integer_)
+    expect_identical(out$XAxis, NA_integer_)
+    expect_identical(out$YAxis, NA_integer_)
+})
+
 # .set_default_assay ----
 
 test_that(".set_default_assay returns 1L there is no logcounts assay", {
+    sce_noLogCount <- sce
+    assays(sce_noLogCount) <- assays(sce_noLogCount)["counts"]
 
-  sce_noLogCount <- sce
-  assays(sce_noLogCount) <- assays(sce_noLogCount)["counts"]
-
-  expect_identical(
-    iSEE:::.set_default_assay(sce_noLogCount),
-    1L
-  )
-
+    expect_identical(iSEE:::.set_default_assay(sce_noLogCount), 1L)
 })
 
 # .override_defaults ----
 
 test_that(".override_defaults warns about unexpected settings", {
+    redDimArgsExtraField <- redDimPlotDefaults(sce, 1)
+    redDimArgsExtraField$DummyExtraField <- NA_character_
 
-  redDimArgsExtraField <- redDimPlotDefaults(sce, 1)
-  redDimArgsExtraField$DummyExtraField <- NA_character_
+    expect_warning(
+        iSEE:::.override_defaults(redDimPlotDefaults(sce, nrow(
+            redDimArgsExtraField
+        )),
+            redDimArgsExtraField),
+        "unknown field .* in user-specified settings"
+    )
 
-  expect_warning(
-    iSEE:::.override_defaults(
-      redDimPlotDefaults(sce, nrow(redDimArgsExtraField)),
-      redDimArgsExtraField
-    ),
-    "unknown field .* in user-specified settings"
-  )
+    # Correctly overwrites default fields.
+    ref <- redDimPlotDefaults(sce, 5)
+    out <- iSEE:::.override_defaults(ref, DataFrame(Type = 2L))
+    expect_identical(out$Type, rep(2:1, c(1, 4)))
 
-  # Correctly overwrites default fields.
-  ref <- redDimPlotDefaults(sce, 5)
-  out <- iSEE:::.override_defaults(ref, DataFrame(Type=2L))
-  expect_identical(out$Type, rep(2:1, c(1,4)))
+    out <- iSEE:::.override_defaults(ref, DataFrame(Type = c(2L, 2L)))
+    expect_identical(out$Type, rep(2:1, c(2, 3)))
 
-  out <- iSEE:::.override_defaults(ref, DataFrame(Type=c(2L, 2L)))
-  expect_identical(out$Type, rep(2:1, c(2,3)))
-
-  # Correctly handles list inputs.
-  zoomed <- c(xmin=1, xmax=2, ymin=5, ymax=5)
-  out <- iSEE:::.override_defaults(ref, DataFrame(ZoomData=I(list(zoomed, zoomed))))
-  expect_identical(out$ZoomData, list(zoomed, zoomed, NULL, NULL, NULL))
+    # Correctly handles list inputs.
+    zoomed <- c(
+        xmin = 1,
+        xmax = 2,
+        ymin = 5,
+        ymax = 5
+    )
+    out <-
+        iSEE:::.override_defaults(ref, DataFrame(ZoomData = I(list(zoomed, zoomed))))
+    expect_identical(out$ZoomData, list(zoomed, zoomed, NULL, NULL, NULL))
 })
 
 
-test_that(".add_general_parameters_for_*_plots functions correctly respond to internal fields", {
-    sce <- iSEE:::.precompute_UI_info(sce, NULL, NULL)
+test_that(
+    ".add_general_parameters_for_*_plots functions correctly respond to internal fields",
+    {
+        sce <- iSEE:::.precompute_UI_info(sce, NULL, NULL)
 
-    out <- iSEE:::.add_general_parameters_for_column_plots(DataFrame(a=1), sce)
-    expect_identical(out[[iSEE:::.facetRowsByColData]], iSEE:::.get_internal_info(sce, "column_groupable")[1])
-    expect_identical(out[[iSEE:::.facetColumnsByColData]], iSEE:::.get_internal_info(sce, "column_groupable")[1])
+        out <-
+            iSEE:::.add_general_parameters_for_column_plots(DataFrame(a = 1), sce)
+        expect_identical(out[[iSEE:::.facetRowsByColData]], iSEE:::.get_internal_info(sce, "column_groupable")[1])
+        expect_identical(out[[iSEE:::.facetColumnsByColData]], iSEE:::.get_internal_info(sce, "column_groupable")[1])
 
-    SingleCellExperiment:::int_metadata(sce)$iSEE["column_groupable"] <- "YAY"
-    out <- iSEE:::.add_general_parameters_for_column_plots(DataFrame(a=1), sce)
-    expect_identical(out[[iSEE:::.facetRowsByColData]], "YAY")
-    expect_identical(out[[iSEE:::.facetColumnsByColData]], "YAY")
+        SingleCellExperiment:::int_metadata(sce)$iSEE["column_groupable"] <-
+            "YAY"
+        out <-
+            iSEE:::.add_general_parameters_for_column_plots(DataFrame(a = 1), sce)
+        expect_identical(out[[iSEE:::.facetRowsByColData]], "YAY")
+        expect_identical(out[[iSEE:::.facetColumnsByColData]], "YAY")
 
-    out <- iSEE:::.add_general_parameters_for_row_plots(DataFrame(b=1), sce)
-    expect_identical(out[[iSEE:::.facetRowsByRowData]], iSEE:::.get_internal_info(sce, "row_groupable")[1])
-    expect_identical(out[[iSEE:::.facetColumnsByRowData]], iSEE:::.get_internal_info(sce, "row_groupable")[1])
+        out <-
+            iSEE:::.add_general_parameters_for_row_plots(DataFrame(b = 1), sce)
+        expect_identical(out[[iSEE:::.facetRowsByRowData]], iSEE:::.get_internal_info(sce, "row_groupable")[1])
+        expect_identical(out[[iSEE:::.facetColumnsByRowData]], iSEE:::.get_internal_info(sce, "row_groupable")[1])
 
-    SingleCellExperiment:::int_metadata(sce)$iSEE["row_groupable"] <- "WHEE"
-    out <- iSEE:::.add_general_parameters_for_row_plots(DataFrame(b=1), sce)
-    expect_identical(out[[iSEE:::.facetRowsByRowData]], "WHEE")
-    expect_identical(out[[iSEE:::.facetColumnsByRowData]], "WHEE")
-})
+        SingleCellExperiment:::int_metadata(sce)$iSEE["row_groupable"] <-
+            "WHEE"
+        out <-
+            iSEE:::.add_general_parameters_for_row_plots(DataFrame(b = 1), sce)
+        expect_identical(out[[iSEE:::.facetRowsByRowData]], "WHEE")
+        expect_identical(out[[iSEE:::.facetColumnsByRowData]], "WHEE")
+    }
+)

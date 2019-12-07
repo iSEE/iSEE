b2dab1d2e1377e838badd7db27ac3ad3e6557533
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2019-12-07 22:29:00 +0000

    reuse .text_eval in .evaluate_commands

--- R/commands.R
+++ R/commands.R
@@ -1,12 +1,12 @@
 #' Manage commands to be evaluated
-#' 
+#'
 #' Functions to manage pending and processed commands to be evaluated.
 #'
 #' @param obj A command store produced by \code{.initialize_cmd_store}.
 #' @param cmd A character vector containing commands to be executed at some point in time.
 #' @param name A character vector containing names for the commands.
 #' @param envir An environment in which to execute the commands.
-#' 
+#'
 #' @return
 #' A command store, i.e., a list of two character vectors \code{pending} and \code{processed}.
 #'
@@ -36,7 +36,7 @@
 #' @rdname INTERNAL_manage_commands
 .evaluate_commands <- function(obj, envir) {
     if (length(obj$pending) && !is.null(envir)) {
-        eval(parse(text=obj$pending), envir=envir)
+        .text_eval(obj$pending, envir=envir)
     }
     obj$processed <- c(obj$processed, obj$pending)
     obj$pending <- character(0)
@@ -45,7 +45,7 @@
 
 #' @rdname INTERNAL_manage_commands
 .text_eval <- function(cmd, envir) {
-    if (length(cmd)) { 
+    if (length(cmd)) {
         return(eval(parse(text=cmd), envir))
     } else {
         return(NULL)

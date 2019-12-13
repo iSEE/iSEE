0c7cdc326d7a0626a38e378821fb21a46df04be5
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2019-12-13 12:15:06 +0000

    roxygenize

--- R/addin.R
+++ R/addin.R
@@ -14,14 +14,22 @@ collect_parents <- function() {
 
 #' Create a new panel file
 #'
-#' Opens a new R script in the editor after substituting
+#' Opens a template R script in the editor, to define a new iSEE panel class .
 #'
-#' @param encoded
-#' @param decoded
-#' @param parent
+#' @param encoded Name of the new panel class.
+#' @param decoded Extended name of the new panel class (for display).
+#' @param parent Name of the parent panel class
 #'
 #' @importFrom rstudioapi documentNew
-new_panel_file <- function(encoded, decoded, parent) {
+#' @export
+#'
+#' @author Kevin Rue-Albrecht
+#'
+#' @seealso \linkS4class{Panel}
+#'
+#' @examples
+#' new_panel_file("NewRedDimPlot", "New reduced dimension plot", "RedDimPlot")
+new_panel_file <- function(encoded, decoded, parent="Panel") {
   template_file <- system.file(package = "iSEE", "templates", "NewPanel.R")
   template_content <- scan(template_file, "character", sep = "\n")
   template_content <- paste0(template_content, collapse = "\n")

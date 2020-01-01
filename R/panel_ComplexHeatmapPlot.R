3103995eb00165ca2e78559282c361e6815e2671
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-01-01 11:08:25 +0100

    Refactor into .generateOutput

--- R/panel_ComplexHeatmapPlot.R
+++ R/panel_ComplexHeatmapPlot.R
@@ -29,32 +29,20 @@ setMethod(".defineOutput", "ComplexHeatmapPlot", function(x) {
 #' @export
 setMethod(".renderOutput", "ComplexHeatmapPlot", function(x, se, output, pObjects, rObjects) {
     plot_name <- .getEncodedName(x)
-    .input_FUN <- function(field) { paste0(plot_name, "_", field) }
 
-    output[[plot_name]] <- renderPlot({
-        force(rObjects[[plot_name]])
+    .create_plot_output(plot_name, se=se, output=output, pObjects=pObjects, rObjects=rObjects)
 
-        p.out <- .make_complexheatmap()
-
-        pObjects$commands[[plot_name]] <- p.out$cmd_list
-
-        p.out$plot
-    })
+    callNextMethod()
 })
 
-#' Makes a complex heatmap
-#'
-#' Make a heatmap with features on the Y axis and samples on the X axis.
-#'
-#' @importFrom ComplexHeatmap Heatmap
-#' @rdname INTERNAL_make_complexheatmap
-.make_complexheatmap <- function() {
-    eval_env <- new.env()
-    plot_cmds <- list()
+setMethod(".generateOutput", "ComplexHeatmapPlot", function(x, se, memory, contents) {
+    plot_env <- new.env()
+    all_cmds <- list()
 
-    plot_cmds[["data"]] <- 'm <- matrix(c(1,2,3, 11,12,13), nrow = 2, ncol = 3, byrow = TRUE, dimnames = list(c("row1", "row2"), c("C.1", "C.2", "C.3")))'
-    plot_cmds[["heatmap"]] <- "Heatmap(m)"
+    all_cmds[["data"]] <- 'm <- matrix(c(1,2,3, 11,12,13), nrow = 2, ncol = 3, byrow = TRUE, dimnames = list(c("row1", "row2"), c("C.1", "C.2", "C.3")))'
+    all_cmds[["heatmap"]] <- "Heatmap(m)"
 
-    plot_out <- .text_eval(plot_cmds, eval_env)
-    list(plot=plot_out, cmd_list=plot_cmds)
-}
+    plot_out <- .text_eval(all_cmds, plot_env)
+
+    list(commands=all_cmds, plot=plot_out)
+})

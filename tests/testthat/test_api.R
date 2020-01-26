b4b4fcffe72c6af50961d1cb72be1e374d864df7
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-01-26 15:44:40 +0000

    .refineParameters returns NULL for NULL input

---
+++ tests/testthat/test_api.R
@@ -0,0 +1,65 @@
+context("API")
+
+test_that("", {
+
+    FUN <- selectMethod(".refineParameters", signature="ColumnDotPlot")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="ColumnTable")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="DotPlot")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="Panel")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="RowDotPlot")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="RowTable")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="Table")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="ColDataPlot")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="ColStatTable")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="ComplexHeatmapPlot")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="FeatAssayPlot")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="RedDimPlot")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="RowDataPlot")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="RowStatTable")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+    FUN <- selectMethod(".refineParameters", signature="SampAssayPlot")
+    out <- FUN(NULL, sce)
+    expect_null(out, NULL)
+
+})

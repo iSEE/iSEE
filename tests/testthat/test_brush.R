09ce9a13bac03b5b042fd78db3bd5c83ffa85705
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-01-28 21:20:07 +0000

    unit test utils_brush.R

--- tests/testthat/test_brush.R
+++ tests/testthat/test_brush.R
@@ -5,14 +5,14 @@ context("brush")
 # .identical_brushes ----
 
 test_that(".identical_brushes works as expected", {
-    example <- list(xmin = 8.0882520175732, xmax = 43.722129933505, ymin = -4.2882334274045, ymax = 39.525409385225, 
-        coords_css = list(xmin = 283.009614944458, xmax = 416.009614944458, ymin = 75.0096130371094, 
-            ymax = 230.009613037109), coords_img = list(xmin = 367.916474130582, xmax = 540.818342036981, 
-            ymin = 97.5124933715051, ymax = 299.012485980529), img_css_ratio = list(x = 1.30001404440901, 
-            y = 1.29999995231628), mapping = list(x = "X", y = "Y"), domain = list(left = -57.2288623709444, 
-            right = 49.1019902822796, bottom = -70.389479254644, top = 53.5190834641125), 
-        range = list(left = 50.986301369863, right = 566.922374429224, bottom = 603.013698630137, 
-            top = 33.1552511415525), log = list(x = NULL, y = NULL), direction = "xy", 
+    example <- list(xmin = 8.0882520175732, xmax = 43.722129933505, ymin = -4.2882334274045, ymax = 39.525409385225,
+        coords_css = list(xmin = 283.009614944458, xmax = 416.009614944458, ymin = 75.0096130371094,
+            ymax = 230.009613037109), coords_img = list(xmin = 367.916474130582, xmax = 540.818342036981,
+            ymin = 97.5124933715051, ymax = 299.012485980529), img_css_ratio = list(x = 1.30001404440901,
+            y = 1.29999995231628), mapping = list(x = "X", y = "Y"), domain = list(left = -57.2288623709444,
+            right = 49.1019902822796, bottom = -70.389479254644, top = 53.5190834641125),
+        range = list(left = 50.986301369863, right = 566.922374429224, bottom = 603.013698630137,
+            top = 33.1552511415525), log = list(x = NULL, y = NULL), direction = "xy",
         brushId = "RedDimPlot1_Brush", outputId = "RedDimPlot1")
 
     expect_true(iSEE:::.identical_brushes(old_brush=NULL, new_brush=NULL))
@@ -82,4 +82,49 @@ test_that(".transmitted_selection detects whether a brush is active", {
     out <- iSEE:::.transmitted_selection("ColDataPlot1", "RedDimPlot1", all_memory,
         select_type="Saved", select_saved=1L)
     expect_true(out)
+})
+
+test_that(".identical_brushes returns FALSE is for two different open lassos", {
+
+    LASSO_OPEN2 <- LASSO_OPEN
+    LASSO_OPEN2$coord <- head(LASSO_OPEN2$coord, 2)
+
+    out <- .identical_brushes(old_brush = LASSO_OPEN, new_brush = LASSO_OPEN2)
+    expect_false(out)
+
+})
+
+test_that(".get_brushed_point returns the identity of selected points or NULL", {
+
+    contents <- data.frame(X=1, Y=seq_len(100), row.names = paste0("X", seq_len(100)))
+
+    # Shiny brush
+    brush_data <- list(
+        xmin = 0.7, xmax = 1.3, ymin = 0.5, ymax = 50.5,
+        mapping = list(x = "X", y = "Y"),
+        log = list(x = NULL, y = NULL), direction = "xy",
+        brushId = "RedDimPlot1_Brush",
+        outputId = "RedDimPlot1")
+    out <- .get_brushed_points(contents, brush_data)
+    expect_identical(out, paste0("X", seq_len(50)))
+
+    # lasso
+    lasso_data <- list(
+        lasso=NULL,
+        closed=TRUE,
+        panelvar1=NULL, panelvar2=NULL,
+        mapping=list(x="X", y="Y"),
+        coord=matrix(c(
+            0.7, 0.5,
+            1.3, 0.5,
+            1.3, 50.5,
+            0.7, 50.5,
+            0.7, 0.5), ncol=2, byrow = TRUE))
+    out <- .get_brushed_points(contents, lasso_data)
+    expect_identical(out, paste0("X", seq_len(50)))
+
+    # NULL if not a brush or a closed lasso
+    out <- .get_brushed_points(contents, list())
+    expect_null(out)
+
 })
--- tests/testthat/test_lasso.R
+++ tests/testthat/test_lasso.R
@@ -3,47 +3,14 @@
 
 context("lasso")
 
-stopifnot(
-  require(mgcv)
-)
-
-LASSO_OPEN <- list(
-    lasso=NULL,
-    closed=FALSE,
-    panelvar1=NULL, panelvar2=NULL,
-    mapping=list(x="X", y="Y"),
-    coord=matrix(c(1, 2, 2, 1, 1, 1, 2, 2), ncol=2))
-
-CLICK_CLOSING <- list(
-    x=1, y=1,
-    mapping=list(x="X", y="Y"),
-    domain=list(left=-14.1, right=10.9, bottom=-12, top=16.4),
-    range=list(left=38.7, right=379, bottom=466, top=23.8),
-    log=list(x=NULL, y=NULL)
-)
-
-CLICK_WAYPOINT <- list(
-    x=3, y=4,
-    mapping=list(x="X", y="Y"),
-    domain=list(left=-14.1, right=10.9, bottom=-12, top=16.4),
-    range=list(left=38.7, right=379, bottom=466, top=23.8),
-    log=list(x=NULL, y=NULL)
-)
-
-LASSO_CLOSED <- list(
-    lasso=NULL,
-    closed=TRUE,
-    panelvar1=NULL, panelvar2=NULL,
-    mapping=list(x="X", y="Y"),
-    coord=matrix(c(1, 2, 2, 1, 1, 1, 1, 2, 2, 1), ncol=2))
-
 DATA_POINTS <- data.frame(
     X=seq(1.05, 2.95, 0.1),
     Y=rep(1.5, 20)
 )
 
-# Run this _after_ the unit tests above
-CLOSED_LASSO <- iSEE:::.update_lasso(CLICK_CLOSING, LASSO_OPEN)
+stopifnot(
+  require(mgcv)
+)
 
 # .update_lasso ----
 

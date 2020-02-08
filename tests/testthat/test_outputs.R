5ecef163a1183e1768c7f076ecfff42d62f4ea47
Your Name <you@example.com>
2020-02-08 19:08:45 +0000

    cover outputs_general.R

--- tests/testthat/test_outputs.R
+++ tests/testthat/test_outputs.R
@@ -39,3 +39,38 @@ test_that(".create_table_output works as expected", {
     expect_is(output[["RowStatTable1"]], "shiny.render.function")
 
 })
+
+# .create_general_output ----
+
+test_that(".create_general_output works as expected", {
+
+    input <- new.env()
+    output <- new.env()
+    session <- new.env()
+    pObjects <- new.env()
+    rObjects <- new.env()
+
+    out <- .create_general_output(sce, input, output, session, pObjects, rObjects)
+
+    expect_null(out)
+    expect_named(output, c("iSEE_INTERNAL_export_content_download", "iSEE_INTERNAL_link_graph_plot",  "iSEE_INTERNAL_memory_export", "iSEE_INTERNAL_export_content_ui" ))
+
+})
+
+# .define_export_choices ----
+
+test_that("define_export_choices supports empty memory", {
+
+    memory <- list()
+
+    out <- .define_export_choices(memory)
+    expect_identical(out, character(0))
+})
+
+test_that("define_export_choices supports non-empty memory", {
+
+    memory <- list(RedDimPlot(PanelId=1L))
+
+    out <- .define_export_choices(memory)
+    expect_identical(out, c("Reduced dimension plot 1" = "RedDimPlot1"))
+})

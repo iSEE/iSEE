25ad45d441feca498f942ecdc82c9ec2b34754ef
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-02-08 11:20:37 -0800

    Added tests to cover new SE-filling code.

--- tests/testthat/test_iSEE-main.R
+++ tests/testthat/test_iSEE-main.R
@@ -26,3 +26,19 @@ test_that("iSEE main function runs with empty colData(sce)", {
     app <- iSEE(sce, runLocal=TRUE)
     expect_s3_class(app, "shiny.appobj")
 })
+
+test_that("iSEE main function runs with empty dimnames", {
+    out <- .fill_se_dimnames(sce)
+    expect_identical(out$se, sce)
+    expect_identical(out$commands, character(0))
+    
+    dimnames(sce) <- NULL
+    app <- iSEE(sce, runLocal=TRUE)
+    expect_s3_class(app, "shiny.appobj")
+
+    out <- .fill_se_dimnames(sce)
+    expect_false(is.null(rownames(out$se)))
+    expect_false(is.null(colnames(out$se)))
+    expect_true(any(grepl("colnames.* <- ", out$commands)))
+    expect_true(any(grepl("rownames.* <- ", out$commands)))
+})

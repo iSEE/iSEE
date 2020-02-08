ac7a847629bb61381015637f686f06dfc00858fe
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-02-08 00:37:02 -0800

    Added missing internal documentation. Robustified to non-memory case. Updated NAMESPACE.

--- R/outputs_general.R
+++ R/outputs_general.R
@@ -1,3 +1,20 @@
+#' Render general output for the app
+#'
+#' Create rendering expressions for \code{\link{iSEE}} outputs that are general to the entire app, not just specific panels.
+#'
+#' @param se A \linkS4class{SummarizedExperiment} object containing the data of interest.
+#' @param input The Shiny input object from the server function.
+#' @param outpu The Shiny outpu object from the server function.
+#' @param session The Shiny session object from the server function.
+#' @param pObjects An environment containing global parameters generated in the \code{\link{iSEE}} app.
+#' @param rObjects A reactive list of values generated in the \code{\link{iSEE}} app.
+#'
+#' @return A \linkS4class{NULL} is invisibly returned
+#' and rendering expressions for general app features are added to \code{output}.
+#'
+#' @author Aaron Lun
+#'
+#' @rdname INTERNAL_create_general_output
 #' @importFrom utils zip
 #' @importFrom shiny downloadHandler renderPlot checkboxGroupInput actionButton
 .create_general_output <- function(se, input, output, session, pObjects, rObjects) {
@@ -43,10 +60,28 @@
             zip(file, files=unlist(all.files))
         }
     )
+
+    invisible(NULL)
 }
 
+#' Define export choices
+#'
+#' Define the available panels that can be chosen for exporting content.
+#'
+#' @param all_memory A list of \linkS4class{Panel}s specifying the current state of the app.
+#'
+#' @return A character vector of length equal to \code{all_memory}, containing the encoded panel names.
+#' The names of this vector are set to the full panel names for display purposes.
+#'
+#' @author Aaron Lun
+#'
+#' @rdname INTERNAL_define_export_choices
 .define_export_choices <- function(all_memory) {
-    all_options <- vapply(all_memory, .getEncodedName, "")
-    names(all_options) <- vapply(all_memory, .getFullName, "")
+    if (length(all_memory)) {
+        all_options <- vapply(all_memory, .getEncodedName, "")
+        names(all_options) <- vapply(all_memory, .getFullName, "")
+    } else {
+        all_options <- character(0)
+    }
     all_options
 }

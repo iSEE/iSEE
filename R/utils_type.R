ff01579dc2d2af45522ddc1c5853b59e73f650eb
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-02-09 02:03:29 -0800

    Fixed heatmap code to work with sparse assays.

--- R/utils_type.R
+++ R/utils_type.R
@@ -5,8 +5,12 @@
 #'
 #' @return A logical scalar indicating whether the assay is numeric.
 #'
+#' @details
+#' The \code{as.matrix} is necessary to account for non-ordinary matrices,
+#' e.g., sparse or file-backed matrices that remain as S4 instances after subsetting.
+#' 
 #' @author Kevin Rue-Albrecht
 #' @rdname INTERNAL_is_assay_numeric
-.is_assay_numeric <- function(i, se){
-    is.numeric(assay(se, i)[0, 0])
+.is_assay_numeric <- function(i, se) {
+    is.numeric(as.matrix(assay(se, i)[0, 0]))
 }

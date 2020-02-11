49c19fcc04fe6076ae929d2068e2d90181e77458
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-02-10 21:42:02 -0800

    Auto-detect arguments from the custom function definition.

--- R/createCustomPanels.R
+++ R/createCustomPanels.R
@@ -7,9 +7,8 @@
 #' @param FUN A function that generates a data.frame or a \link{ggplot},
 #' for \code{createCustomTable} and \code{createCustomPlot} respectively.
 #' See Details for the expected arguments.
-#' @param argStrings Character vector of names of optional string arguments to \code{FUN}.
-#' @param argNumbers Character vector of names of optional (scalar) numeric arguments to \code{FUN}.
-#' @param argFlags Character vector of names of optional (scalar) logical arguments to \code{FUN}.
+#' @param restrict Character vector of names of optional arguments in \code{FUN} to which the UI is restricted.
+#' If specified, only the listed arguments receive UI elements in the interface.
 #' @param className String containing the name of the new \linkS4class{Panel} class.
 #' @param fullName String containing the full name of the new class.
 #' @param where An environment indicating where the class and method definitions should be stored.
@@ -31,9 +30,17 @@
 #' }
 #'
 #' Any number of additional named arguments may also be present in \code{FUN}.
-#' These will be passed from UI elements in the panel if the names of those arguments are listed in \code{argStrings}, etc.
-#' All such additional arguments should have default values,
-#' which are extracted for use as the default vfalues in the UI.
+#' All such arguments should have default values,
+#' as these are used to automatically generate UI elements in the panel: 
+#' \itemize{
+#' \item Character vectors will get a \code{\link{selectInput}}.
+#' \item Strings will get a \code{\link{textInput}}.
+#' \item Numeric scalars will get a \code{\link{numericInput}}.
+#' \item Logical scalars will get a \code{\link{checkboxInput}}.
+#' }
+#' Arguments with other types of default values are ignored.
+#' If \code{restrict} is specified, arguments will only have corresponding UI elements if they are listed in \code{restrict}.
+#' All user interactions with these elements will automatically trigger regeneration of the panel contents.
 #'
 #' Classes created via these functions are extremely limited.
 #' Only scalar inputs are supported via the UI and all panels cannot transmit to the rest of the app.
@@ -42,10 +49,10 @@
 #'
 #' @examples
 #' library(scater)
-#' CUSTOM_PCA <- function(se, rows, columns, ntop=500, scale=TRUE) {
-#'     if (!is.null(columns)) {
-#'         kept <- se[, unique(unlist(columns))]
-#'     } else {
+#' CUSTOM_DIMRED <- function(se, rows, columns, ntop=500, scale=TRUE,
+#'     mode=c("PCA", "TSNE", "UMAP")) 
+#' {
+#'     if (is.null(columns)) {
 #'         return(
 #'             ggplot() + theme_void() + geom_text(
 #'                 aes(x, y, label=label),
@@ -54,18 +61,22 @@
 #'             )
 #'     }
 #' 
-#'     if (!is.null(rows)) {
-#'         subset_row <- unique(unlist(rows))
-#'     } else {
-#'         subset_row <- NULL
+#'     mode <- match.arg(mode)
+#'     if (mode=="PCA") {
+#'         calcFUN <- runPCA
+#'     } else if (mode=="TSNE") {
+#'         calcFUN <- runTSNE
+#'     } else if (mode=="UMAP") {
+#'         calcFUN <- runUMAP
 #'     }
 #'
-#'     kept <- runPCA(kept, ncomponents=2, ntop=ntop, 
-#'         scale=scale, subset_row=subset_row)
-#'     plotPCA(kept)
+#'     kept <- se[, unique(unlist(columns))]
+#'     kept <- calcFUN(kept, ncomponents=2, ntop=ntop, 
+#'         scale=scale, subset_row=unique(unlist(rows)))
+#'     plotReducedDim(kept, mode)
 #' }
 #'
-#' GEN <- createCustomPlot(CUSTOM_PCA, argNumbers="ntop", argFlags="scale")
+#' GEN <- createCustomPlot(CUSTOM_DIMRED)
 #' GEN()
 #'
 #' if (interactive()) {
@@ -84,26 +95,20 @@
 #' 
 #' @export
 #' @name createCustomPanels
-createCustomTable <- function(FUN, 
-    argStrings=character(0), argNumbers=character(0), argFlags=character(0),
-    className="CustomTable", fullName="Custom table", where=topenv(parent.frame()))
+createCustomTable <- function(FUN, restrict=NULL, className="CustomTable", 
+    fullName="Custom table", where=topenv(parent.frame()))
 {
-    collated <- character(0)
-    collated[argStrings] <- "character"
-    collated[argNumbers] <- "numeric"
-    collated[argFlags] <- "logical"
-
+    fn_args <- .grab_all_args(FUN, restrict)
+    collated <- vapply(fn_args, class, "")
     generator <- setClass(className, contains="Table", slots=collated, where=where)
 
-    .spawn_custom_methods(FUN, className=className, fullName=fullName,
-        argStrings=argStrings, argNumbers=argNumbers, argFlags=argFlags,
-        where=where)
+    .spawn_custom_methods(fn_args, className=className, fullName=fullName, where=where)
 
     fn_name <- deparse(substitute(FUN))
     setMethod(".generateTable", className, function(x, envir) {
         .execute_custom_function(x, FUN, 
             fn_name=fn_name, assigned="tab", envir=envir,
-            fn_args=c(argStrings, argNumbers, argFlags))
+            fn_args=names(fn_args))
     }, where=where)
 
     generator
@@ -111,20 +116,14 @@ createCustomTable <- function(FUN,
 
 #' @export
 #' @rdname createCustomPanels
-createCustomPlot <- function(FUN,
-    argStrings=character(0), argNumbers=character(0), argFlags=character(0),
-    className="CustomPlot", fullName="Custom plot", where=topenv(parent.frame()))
+createCustomPlot <- function(FUN, restrict=NULL, className="CustomPlot", 
+    fullName="Custom plot", where=topenv(parent.frame()))
 {
-    collated <- character(0)
-    collated[argStrings] <- "character"
-    collated[argNumbers] <- "numeric"
-    collated[argFlags] <- "logical"
-
+    fn_args <- .grab_all_args(FUN, restrict)
+    collated <- vapply(fn_args, class, "")
     generator <- setClass(className, contains="Panel", slots=collated, where=where)
 
-    .spawn_custom_methods(FUN, className=className, fullName=fullName,
-        argStrings=argStrings, argNumbers=argNumbers, argFlags=argFlags,
-        where=where)
+    .spawn_custom_methods(fn_args, className=className, fullName=fullName, where=where)
 
     setMethod(".defineOutput", className, function(x) {
         plotOutput(.getEncodedName(x))
@@ -138,9 +137,9 @@ createCustomPlot <- function(FUN,
         selected <- .processMultiSelections(x, all_memory, all_contents, plot_env)
         commands <- .execute_custom_function(x, FUN, 
             fn_name=fn_name, assigned="gg", envir=plot_env,
-            fn_args=c(argStrings, argNumbers, argFlags))
+            fn_args=names(fn_args))
+
         commands <- sub("^gg <- ", "", commands) # to avoid an unnecessary variable.
-        
         list(contents=plot_env$gg, commands=list(select=selected, plot=commands))
     }, where=where)
 
@@ -156,36 +155,62 @@ createCustomPlot <- function(FUN,
     generator
 }
 
-#' @importFrom shiny tagList textInput numericInput checkboxInput
-.spawn_custom_methods <- function(FUN, className, fullName,
-    argStrings=character(0), argNumbers=character(0), argFlags=character(0),
-    where=topenv(parent.frame()))
-{
-    defaults <- formals(FUN)
+#' Internal custom panel methods
+#'
+#' @param defaults A named list of default arguments to the custom function.
+#' @inheritParams createCustomPanels
+#' @param x An instance of the custom \linkS4class{Panel} class.
+#' @param fn_args Character vector of names of all arguments to pass from \code{x} to \code{FUN}.
+#' @param fn_name String containing the name of the function, to show in the code tracker.
+#' @param assigned String containing the name of the variable to assign the output of \code{FUN} during evaluation.
+#' @param envir The evaluation environment.
+#'
+#' @return
+#' \code{.spawn_custom_methods} will define methods for the custom class in \code{where}.
+#'
+#' \code{.grab_all_args} will return a named list of default values from arguments of \code{FUN},
+#' which can be converted to supported UI elements.
+#'
+#' \code{.execute_custom_function} will execute \code{FUN} with the specified arguments from \code{x} and \code{fn_args},
+#' returning a string of R commands to obtain call.
+#'
+#' @author Aaron Lun
+#'
+#' @rdname INTERNAL_custom_panel_methods
+#' @importFrom shiny tagList textInput numericInput checkboxInput selectInput
+.spawn_custom_methods <- function(defaults, className, fullName, where=topenv(parent.frame())) {
+    force(defaults)
+    force(fullName)
+
     setMethod("initialize", className, function(.Object, ...) {
-        args <- list(...)
-        for (x in c(argStrings, argNumbers, argFlags)) {
-            args <- .empty_default(args, x, defaults[[x]])
+        extra_args <- list(...)
+        for (i in names(defaults)) {
+            extra_args <- .empty_default(extra_args, i, defaults[[i]][1]) # select first element when multiple choice.
         }
-        do.call(callNextMethod, c(list(.Object), args))
+        do.call(callNextMethod, c(list(.Object), extra_args))
     }, where=where)
 
     setMethod(".defineDataInterface", className, function(x, se, select_info) {
         tab_name <- .getEncodedName(x)
+        collected <- list()
 
-        string_ui <- lapply(argStrings, function(id) {
-            textInput(paste0(tab_name, "_", id),  label=id, value=x[[id]])
-        })
+        for (i in names(defaults)) {
+            options <- defaults[[i]]
+            current <- x[[i]]
+            id <- paste0(tab_name, "_", i)
 
-        number_ui <- lapply(argNumbers, function(id) {
-            numericInput(paste0(tab_name, "_", id), label=id, value=x[[id]])
-        })
+            collected[[i]] <- if (is.character(options) && length(options)==1L) {
+                textInput(id, label=i, value=current)
+            } else if (is.character(options) && length(options) >= 1L) {
+                selectInput(id, label=i, choices=options, selected=current)
+            } else if (is.numeric(options)) {
+                numericInput(id, label=i, value=current)
+            } else if (is.logical(options)) {
+                checkboxInput(id, label=i, value=current)
+            }
+        }
 
-        flag_ui <- lapply(argFlags, function(id) {
-            checkboxInput(paste0(tab_name, "_", id), label=id, value=x[[id]])
-        })
-
-        do.call(tagList, c(string_ui, number_ui, flag_ui))
+        do.call(tagList, collected)
     }, where=where)
 
     setMethod(".createObservers", className, function(x, se, input, session, pObjects, rObjects) {
@@ -193,7 +218,7 @@ createCustomPlot <- function(FUN,
 
         # Doesn't matter all that much whether they're protected or not,
         # given that custom panels cannot transmit.
-        .createProtectedParameterObservers(panel_name, c(argStrings, argNumbers, argFlags),
+        .createProtectedParameterObservers(panel_name, names(defaults),
             input=input, pObjects=pObjects, rObjects=rObjects)
     }, where=where)
 
@@ -202,6 +227,31 @@ createCustomPlot <- function(FUN,
     setMethod(".panelColor", className, function(x) "#4D4D4D", where=where)
 }
 
+#' @rdname INTERNAL_custom_panel_methods
+.grab_all_args <- function(FUN, restrict=NULL) {
+    default.args <- formals(FUN)
+    default.args <- default.args[-seq_len(3)]
+
+    # Prune out arguments that we can't support.
+    keepers <- list()
+    for (i in names(default.args)) {
+        current <- eval(default.args[[i]])
+        if ((is.character(current) && length(current)!=0L) ||
+            (is.numeric(current) && length(current)==1L) ||
+            (is.logical(current) && length(current)==1L))
+        {
+            keepers[[i]] <- current
+        }
+    }
+
+    if (!is.null(restrict)) {
+        keepers <- keepers[intersect(names(keepers), restrict)]
+    }
+
+    keepers
+}
+
+#' @rdname INTERNAL_custom_panel_methods
 .execute_custom_function <- function(x, FUN, fn_name, fn_args, assigned, envir) {
     fn_call <- paste(assigned, "<- %s(se,") 
 

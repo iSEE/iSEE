d1169a319e7c029ddb729e90951aa89216c539c3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-02-24 21:47:42 -0800

    Extended subsetting code to handle different priority levels.

--- tests/testthat/test_subset_grid.R
+++ tests/testthat/test_subset_grid.R
@@ -1,7 +1,7 @@
 context("subset_points")
 
 # This tests the subsetPointsByGrid function.
-# library(testthat); library(iSEE); source("test_subset_points.R")
+# library(testthat); library(iSEE); source("test_subset_grid.R")
 
 set.seed(110001)
 test_that("subsetPointsByGrid works correctly", {
@@ -27,4 +27,35 @@ test_that("subsetPointsByGrid works correctly", {
 
     # Checking silly inputs.
     expect_identical(suppressWarnings(subsetPointsByGrid(integer(0L), integer(0L))), logical(0L))
+    expect_error(subsetPointsByGrid(1L, integer(0L)), "must be of the same length")
 })
+
+set.seed(110001)
+test_that("subsetPointsByGrid works correctly with grouping", {
+    x <- rnorm(20000)
+    y <- rnorm(20000)
+    g <- sample(letters[1:5], length(x), replace=TRUE)
+
+    chosen <- subsetPointsByGrid(x, y, resolution=200, grouping=g)
+    for (ug in unique(g)) {
+        keep <- g==ug
+        expect_identical(
+            chosen[keep],
+            subsetPointsByGrid(x[keep], y[keep], resolution=200)
+        )
+    }
+
+    multires <- c(a=100, b=200, c=300, d=50, e=20)
+    chosen <- subsetPointsByGrid(x, y, resolution=multires, grouping=g)
+    for (ug in unique(g)) {
+        keep <- g==ug
+        expect_identical(
+            chosen[keep],
+            subsetPointsByGrid(x[keep], y[keep], resolution=multires[[ug]])
+        )
+    }
+
+    expect_error(subsetPointsByGrid(x, y, grouping=1), "must be of the same length")
+    expect_error(subsetPointsByGrid(x, y, grouping=g, resolution=c(A=1, B=2)), "named with all levels")
+})
+

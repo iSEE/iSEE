45b8c76e196efc544b4e8ae2b5d21a02c3e9082c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-10-08 14:42:39 -0700

    Ensure that initialized is set to TRUE for child observers.

--- R/observers_child.R
+++ R/observers_child.R
@@ -56,10 +56,17 @@
 #' @author Aaron Lun
 #'
 #' @rdname INTERNAL_child_propagation_observer
-#' @importFrom shiny observeEvent
+#' @importFrom shiny observeEvent onFlushed
 #' @importFrom igraph topo_sort adjacent_vertices
 .create_child_propagation_observer <- function(se, pObjects, rObjects) {
     # nocov start
+
+    # Run this just in case we haven't triggered the observer below on start-up 
+    # (in which case the app will refuse to respond to the # first user input).
+    # This occasionally occurs for very well-behaved Panels that do not trigger
+    # further changes to their 'input' fields upon initialization.
+    onFlushed(function() pObjects$initialized <- TRUE)
+
     observeEvent(rObjects$modified, {
         if (!isTRUE(pObjects$initialized)) { # Avoid running this on app start and double-generating output.
             pObjects$initialized <- TRUE

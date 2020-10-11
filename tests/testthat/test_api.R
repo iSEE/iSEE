423bbd034ccae5b84d046f55b6b27f109e517f67
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-10-11 23:29:28 +0100

    No need for NULL in expect_null (#496)

--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -15,7 +15,7 @@ test_that(".refineParameters handles NULL x", {
     for (x_class in x_classes) {
         FUN <- selectMethod(".refineParameters", signature=x_class)
         out <- FUN(NULL, sce)
-        expect_null(out, NULL)
+        expect_null(out)
     }
 
 })
@@ -391,26 +391,26 @@ test_that(".exportOutput handles Table", {
 })
 
 test_that(".exportOutput handles ComplexHeatmapPlot", {
-    
+
     ComplexHeatmapPlot1 <- ComplexHeatmapPlot(PanelId=1L)
     sce <- .cacheCommonInfo(ComplexHeatmapPlot1, sce)
     ComplexHeatmapPlot1 <- .refineParameters(ComplexHeatmapPlot1, sce)
     memory <- list(ComplexHeatmapPlot1=ComplexHeatmapPlot1)
     pObjects <- mimic_live_app(sce, memory)
     metadata(sce)$colormap <- ExperimentColorMap()
-    
+
     out <- .exportOutput(memory$ComplexHeatmapPlot1, sce, memory, pObjects$contents)
     expect_identical(out, "ComplexHeatmapPlot1.pdf")
-    
+
 })
 
 test_that(".exportOutput handles Panel", {
-    
+
     panel1 <- new("PanelChildClass")
-    
+
     out <- .exportOutput(panel1, sce, list(), list())
     expect_identical(out, character(0))
-    
+
 })
 
 # .defineVisualShapeInterface ----

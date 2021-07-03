0656713a754435df45e49c5a15a058534c9c7e45
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-07-03 19:46:57 +0200

    fix tests to run manually

--- tests/testthat/test_outputs.R
+++ tests/testthat/test_outputs.R
@@ -1,4 +1,4 @@
-# outputs_info.R ----
+# library(iSEE); library(testthat); source("setup_sce.R"); source("test_outputs.R")
 context("Info")
 
 test_that(".create_selection_info_output works as expected", {
@@ -7,7 +7,7 @@ test_that(".create_selection_info_output works as expected", {
     pObjects = new.env()
     rObjects = new.env()
 
-    out <- .create_selection_info_output("ReducedDimensionPlot", sce, output, pObjects, rObjects)
+    out <- iSEE:::.create_selection_info_output("ReducedDimensionPlot", sce, output, pObjects, rObjects)
     expect_null(out)
     expect_is(output[["ReducedDimensionPlot_INTERNAL_PanelMultiSelectInfo"]], "shiny.render.function")
 
@@ -19,7 +19,7 @@ test_that(".create_link_info_output works as expected", {
     pObjects = new.env()
     rObjects = new.env()
 
-    out <- .create_link_info_output("ReducedDimensionPlot", output, pObjects, rObjects)
+    out <- iSEE:::.create_link_info_output("ReducedDimensionPlot", output, pObjects, rObjects)
     expect_null(out)
     expect_is(output[["ReducedDimensionPlot_INTERNAL_PanelSelectLinkInfo"]], "shiny.render.function")
 
@@ -34,7 +34,7 @@ test_that(".create_table_output works as expected", {
     pObjects <- new.env()
     rObjects <- new.env()
 
-    out <- .create_table_output("RowDataTable1", sce, output, pObjects, rObjects)
+    out <- iSEE:::.create_table_output("RowDataTable1", sce, output, pObjects, rObjects)
     expect_null(out)
     expect_is(output[["RowDataTable1"]], "shiny.render.function")
 
@@ -50,7 +50,7 @@ test_that(".create_general_output works as expected", {
     pObjects <- new.env()
     rObjects <- new.env()
 
-    out <- .create_general_output(sce, input, output, session, pObjects, rObjects)
+    out <- iSEE:::.create_general_output(sce, input, output, session, pObjects, rObjects)
 
     expect_null(out)
     expect_named(output, c("iSEE_INTERNAL_export_content_download", "iSEE_INTERNAL_link_graph_plot",  "iSEE_INTERNAL_memory_export", "iSEE_INTERNAL_export_content_ui" ))
@@ -63,7 +63,7 @@ test_that("define_export_choices supports empty memory", {
 
     memory <- list()
 
-    out <- .define_export_choices(memory)
+    out <- iSEE:::.define_export_choices(memory)
     expect_identical(out, character(0))
 })
 
@@ -71,6 +71,6 @@ test_that("define_export_choices supports non-empty memory", {
 
     memory <- list(ReducedDimensionPlot(PanelId=1L))
 
-    out <- .define_export_choices(memory)
+    out <- iSEE:::.define_export_choices(memory)
     expect_identical(out, c("Reduced dimension plot 1" = "ReducedDimensionPlot1"))
 })

65ff74c1150bd8a9d26d250d62c36f8175e84f12
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-10-04 14:33:39 +0100

    document .process_tooltip_field

--- R/utils_tooltip.R
+++ R/utils_tooltip.R
@@ -4,18 +4,25 @@
 #' Typically, from the \code{rownames} or \code{colnames}.
 #' @param fields Named list of fields to include in the tooltip text.
 #' See \emph{Details}.
+#' @param value Scalar representing one value in \code{fields}.
 #' 
 #' @details 
-#' Every item in the argument \code{fields} is added as a new line in the tooltip.
+#' Every item in the argument \code{fields} is added as a separate line in the tooltip.
 #' The line is formatted as \dQuote{\bold{name:} value}.
 #'
-#' @return An \code{\link{HTML}} element that will be displayed in the tooltip.
+#' @return 
+#' \code{.generate_tooltip_html} returns an \code{\link{HTML}} element
+#' that will be displayed in the tooltip.
+#' 
+#' \code{.process_tooltip_field} converts individual fields to a character
+#' representation suitable for display. For instance, this may include trimming
+#' double-precision scalars to a set number of significant digits.
 #'
 #' @author Kevin Rue-Albrecht
 #'
 #' @rdname INTERNAL_generate_tooltip_html
 .generate_tooltip_html <- function(name, fields) {
-    fields <- sapply(fields, function(x) .process_tooltip_field(x))
+    fields <- sapply(fields, function(x) .process_tooltip_field(x), USE.NAMES = TRUE)
     HTML(
         paste0(c(
             sprintf("<strong>%s</strong>", name),
@@ -24,9 +31,18 @@
         )
 }
 
-.process_tooltip_field <- function(x) {
-    if (is.double(x)) {
-        x <- signif(x, digits = getAppOption("tooltip.signif", default = 6))
+#' @rdname INTERNAL_generate_tooltip_html
+.process_tooltip_field <- function(value) {
+    original <- value
+    if (is.double(value)) {
+        value <- signif(value, digits = getAppOption("tooltip.signif", default = 6))
+        value <- ifelse(
+            identical(value, original),
+            as.character(value),
+            paste0(as.character(value), "(...)")
+            )
+    } else if (is.factor(value)) {
+        value <- as.character(value)
     }
-    x
+    value
 }

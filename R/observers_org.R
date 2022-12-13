6a483d251e708306e3b0986cab87aef8ce994514
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-12-13 09:55:30 +0000

    refactor constants and slot names in observers_org.R

--- R/observers_org.R
+++ R/observers_org.R
@@ -189,7 +189,7 @@
 .create_width_height_observers <- function(x, input, org_pObjects) {
     panel_name <- .getEncodedName(x)
 
-    width_name <- paste0(panel_name, "_", .organizationWidth)
+    width_name <- paste0(panel_name, "_", iSEEslots$organizationWidth)
 
     # nocov start
     observeEvent(input[[width_name]], {
@@ -197,15 +197,15 @@
             return(NULL)
         }
 
-        cur.width <- slot(org_pObjects$memory[[panel_name]], .organizationWidth)
+        cur.width <- slot(org_pObjects$memory[[panel_name]], iSEEslots$organizationWidth)
         new.width <- as.integer(input[[width_name]])
         if (!isTRUE(all.equal(new.width, cur.width))) {
-            slot(org_pObjects$memory[[panel_name]], .organizationWidth) <- new.width
+            slot(org_pObjects$memory[[panel_name]], iSEEslots$organizationWidth) <- new.width
         }
     }, ignoreInit=TRUE)
     # nocov end
 
-    height_name <- paste0(panel_name, "_", .organizationHeight)
+    height_name <- paste0(panel_name, "_", iSEEslots$organizationHeight)
 
     # nocov start
     observeEvent(input[[height_name]], {
@@ -213,10 +213,10 @@
             return(NULL)
         }
 
-        cur.height <- slot(org_pObjects$memory[[panel_name]], .organizationHeight)
+        cur.height <- slot(org_pObjects$memory[[panel_name]], iSEEslots$organizationHeight)
         new.height <- as.integer(input[[height_name]])
         if (!isTRUE(all.equal(new.height, cur.height))) {
-            slot(org_pObjects$memory[[panel_name]], .organizationHeight) <- new.height
+            slot(org_pObjects$memory[[panel_name]], iSEEslots$organizationHeight) <- new.height
         }
     }, ignoreInit=TRUE)
     # nocov end
